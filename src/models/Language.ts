9c4e52358f0f71db8238a056acb0fa1d2386222e
Noah Boos <nboosgamemaker@gmail.com>
2025-09-30 00:26:06 +0200

    Feat. - Added `Language` model.

---
+++ src/models/Language.ts
@@ -0,0 +1,65 @@
+export class Language {
+    private readonly id: number;
+    private iso_639_1: string;
+    private iso_639_3: string;
+    private is_conlang: boolean;
+    private name_native: string;
+    private name_local: string;
+    private direction: string;
+
+    constructor(id: number, iso_639_1: string, iso_639_3: string, is_conlang: boolean, name_native: string, name_local: string, direction: string) {
+        this.id = id;
+        this.iso_639_1 = iso_639_1;
+        this.iso_639_3 = iso_639_3;
+        this.is_conlang = is_conlang;
+        this.name_native = name_native;
+        this.name_local = name_local;
+        this.direction = direction;
+    }
+
+    public GetId(): number {
+        return this.id;
+    }
+
+    public GetIso639_1(): string {
+        return this.iso_639_1;
+    }
+    public SetIso639_1(iso_639_1: string) {
+        this.iso_639_1 = iso_639_1;
+    }
+
+    public GetIso639_3(): string {
+        return this.iso_639_3;
+    }
+    public SetIso639_3(iso_639_3: string) {
+        this.iso_639_3 = iso_639_3;
+    }
+
+    public GetIsConlang(): boolean {
+        return this.is_conlang;
+    }
+    public SetIsConlang(is_conlang: boolean) {
+        this.is_conlang = is_conlang;
+    }
+
+    public GetNameNative(): string {
+        return this.name_native;
+    }
+    public SetNameNative(name_native: string) {
+        this.name_native = name_native;
+    }
+
+    public GetNameLocal(): string {
+        return this.name_local;
+    }
+    public SetNameLocal(name_local: string) {
+        this.name_local = name_local;
+    }
+
+    public GetDirection(): string {
+        return this.direction;
+    }
+    public SetDirection(direction: string) {
+        this.direction = direction;
+    }
+}
