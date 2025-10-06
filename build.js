9293f8728c3ff500606416e8e5a30d89bed02365
Noah Boos <nboosgamemaker@gmail.com>
2025-10-06 15:44:37 +0200

    Refacto. - `build.js` now bulk-process the `src/views` directory and filter to only process `.html` files.

--- build.js
+++ build.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
 try {
-    fs.copyFileSync("./src/views/pages/index/index.html", "./dist/views/pages/index/index.html");
+    // fs.copyFileSync("./src/views/pages/index/index.html", "./dist/views/pages/index/index.html");
+    fs.cp("./src/views", "./dist/views", {
+        recursive: true,
+        filter: (src) => src.endsWith(".html") || fs.statSync(src).isDirectory(),
+    }, () => {});
 } catch (error) {
     console.log(error);
 }
