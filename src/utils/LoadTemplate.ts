3b27ed065754d45ce5300fa06e452ddf0c7bd18f
Noah Boos <nboosgamemaker@gmail.com>
2025-10-06 17:56:28 +0200

    Feat. - Added an error handler to `LoadTemplate()`.

--- src/utils/LoadTemplate.ts
+++ src/utils/LoadTemplate.ts
@@ -1,13 +1,16 @@
 export async function LoadTemplate(path: string): Promise<Element | undefined> {
     try {
         const response = await fetch(`/src/views/components/${path}.html`);
-        if(!response.ok) {
+        if (!response.ok) {
             throw new Error(response.statusText);
         }
         const parser: DOMParser = new DOMParser();
         const html: string = await response.text();
         const parsedDocument: Document = parser.parseFromString(html, "text/html");
         const template: Element = parsedDocument.body.firstElementChild!;
+        if (!template) {
+            throw new Error("The requested template can't be parsed to HTML.");
+        }
         return template;
     } catch (error) {
         console.error(`Impossible to load the requested template. \n${error}`);
