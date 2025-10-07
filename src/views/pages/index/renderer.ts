4fbcdd886a2dfc363e6994f6bc523623e49c6a85
Noah Boos <nboosgamemaker@gmail.com>
2025-10-07 23:21:28 +0200

    Feat. - Added language creation form behavior.

--- src/views/pages/index/renderer.ts
+++ src/views/pages/index/renderer.ts
@@ -49,6 +49,48 @@ async function CreateAndHandleLanguageDrawer(leftLeaf: HTMLElement, rightLeaf: H
         const languageFormButton: HTMLButtonElement = languageDrawer!.querySelector<HTMLButtonElement>("#language-form-button")!;
         languageFormButton?.addEventListener("click", async (event: Event) => {
             event.preventDefault();
+            await CreateAndHandleLanguageForm(rightLeaf);
+        })
+    }
+}
+
+async function CreateAndHandleLanguageForm(rightLeaf: HTMLElement) {
+    const languageCreationForm: Element | undefined = await ParseHTMLFromString("forms/language");
+    if (languageCreationForm) {
+        rightLeaf.replaceChildren(languageCreationForm);
+
+        const button: HTMLButtonElement = languageCreationForm!.querySelector<HTMLButtonElement>("#submit")!;
+        button?.addEventListener("click", async (event: Event) => {
+            event.preventDefault();
+            let languageToCreate: { iso_639_1: string, iso_639_3: string, is_conlang: boolean, name_native: string, name_local: string, direction: string } = {
+                iso_639_1: "", iso_639_3: "", is_conlang: false, name_native: "", name_local: "", direction: "ltr"
+            }
+            const inputISO6391: string = languageCreationForm!.querySelector<HTMLInputElement>("#iso_639_1")!.value ?? "";
+            const inputISO6393: string = languageCreationForm!.querySelector<HTMLInputElement>("#iso_639_3")!.value ?? "";
+            const inputIsConlang: boolean = languageCreationForm!.querySelector<HTMLInputElement>("#is_conlang")!.checked ?? false;
+            const inputNameNative: string = languageCreationForm!.querySelector<HTMLInputElement>("#name_native")!.value ?? "";
+            const inputNameLocal: string = languageCreationForm!.querySelector<HTMLInputElement>("#name_local")!.value ?? "";
+            const inputDirection: string = languageCreationForm!.querySelector<HTMLInputElement>("#direction")!.value ?? "";
+            if (inputNameNative == "" || inputNameLocal == "") return;
+            languageToCreate.iso_639_1 = inputISO6391;
+            languageToCreate.iso_639_3 = inputISO6393;
+            languageToCreate.is_conlang = inputIsConlang;
+            languageToCreate.name_native = inputNameNative;
+            languageToCreate.name_local = inputNameLocal;
+            languageToCreate.direction = inputDirection;
+            const success: boolean = await window.txnmAPI.repositories.language.Create(languageToCreate);
+            if (success) {
+                const inputs = languageCreationForm!.querySelectorAll<HTMLInputElement>("input")!;
+                inputs.forEach((input: HTMLInputElement) => {
+                    if (input.type === "checkbox") {
+                        input.checked = false;
+                    } else if (input.type === "text") {
+                        input.value = "";
+                    } else if (input.id === "direction") {
+                        input.value = "ltr";
+                    }
+                })
+            }
         })
     }
 }
