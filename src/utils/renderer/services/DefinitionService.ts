289908812cd8724948704a32cf73b1d1ea536979
Noah Boos <nboosgamemaker@gmail.com>
2025-10-19 16:34:21 +0200

    Feat. - Added `DefinitionService`.

---
+++ src/utils/renderer/services/DefinitionService.ts
@@ -0,0 +1,24 @@
+import {Definition} from "../../../database/models/Definition";
+
+export class DefinitionService {
+    public static async ReadAll(): Promise<Definition[]> {
+        const rawDefinitions: Definition[] = await window.txnmAPI.repositories.definition.ReadAll();
+        return rawDefinitions.map((rawDefinition: Definition): Definition => Definition.Hydrate(rawDefinition));
+    }
+
+    public static async ReadOne(definitionId: number): Promise<Definition> {
+        const rawDefinition: Definition = await window.txnmAPI.repositories.definition.ReadOne(definitionId);
+        return Definition.Hydrate(rawDefinition);
+    }
+
+    public static async Save(definition: Definition): Promise<[boolean, Definition | undefined]> {
+        let [success, savedDefinition] = definition.GetId() == 0
+            ? await window.txnmAPI.repositories.definition.Create(definition)
+            : await window.txnmAPI.repositories.definition.Update(definition);
+        return [success, Definition.Hydrate(savedDefinition)];
+    }
+
+    public static async Delete(definition: Definition): Promise<boolean> {
+        return await window.txnmAPI.repositories.definition.Delete(definition);
+    }
+}
