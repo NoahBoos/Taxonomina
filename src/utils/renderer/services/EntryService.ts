17578bbd839240517879d9b948483d4b08394fd7
Noah Boos <nboosgamemaker@gmail.com>
2025-10-20 21:51:47 +0200

    Feat. - Added `EntryService` methods invoking the new API demands.

--- src/utils/renderer/services/EntryService.ts
+++ src/utils/renderer/services/EntryService.ts
@@ -1,9 +1,20 @@
 import {Entry} from "../../../database/models/Entry";
+import {Definition} from "../../../database/models/Definition";
 
 export class EntryService {
     public static async ReadAll(): Promise<Entry[]> {
         const rawEntries: Entry[] = await window.txnmAPI.repositories.entry.ReadAll();
         return rawEntries.map((rawEntry: Entry): Entry => Entry.Hydrate(rawEntry));
+    }
+
+    public static async ReadAllByGlobalTranslation(rawEntry: Entry): Promise<Entry[]> {
+        const rawEntries: Entry[] = await window.txnmAPI.repositories.entry.ReadAllByGlobalTranslation(rawEntry);
+        return rawEntries.map((rawEntry: Entry) => Entry.Hydrate(rawEntry));
+    }
+
+    public static async ReadAllByLocalTranslation(rawDefinition: Definition): Promise<Entry[]> {
+        const rawEntries: Entry[] = await window.txnmAPI.repositories.entry.ReadAllByLocalTranslation(rawDefinition);
+        return rawEntries.map((rawEntry: Entry) => Entry.Hydrate(rawEntry));
     }
 
     public static async ReadOne(entryId: number): Promise<Entry> {
