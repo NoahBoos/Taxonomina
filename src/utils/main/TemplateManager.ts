22ac9f1adb64cfb5f9ebe29b5b5d22703c583206
Noah Boos <nboosgamemaker@gmail.com>
2025-10-31 15:20:14 +0100

    Refacto. - Deleted useless `console.log()`.

--- src/utils/main/TemplateManager.ts
+++ src/utils/main/TemplateManager.ts
@@ -2,12 +2,9 @@ import path from "node:path";
 import {readFile} from "node:fs/promises";
 
 export async function LoadTemplateAsString(templatePath: string): Promise<string | undefined> {
-    console.log(`Given path : ${templatePath}`);
     try {
         const absolutePath = path.resolve(__dirname, `../../views/components/${templatePath}.html`);
-        console.log(`Absolute path : ${absolutePath}`);
         const content: string = await readFile(absolutePath, "utf8");
-        console.log(content);
         return content;
     } catch (error) {
         console.error(`Impossible to load the requested template. \n`, error);
