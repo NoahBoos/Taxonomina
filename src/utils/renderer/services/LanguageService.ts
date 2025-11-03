2f011ac6a797b7b614fbb830344d9cd20b01855a
Noah Boos <nboosgamemaker@gmail.com>
2025-11-03 09:52:03 +0100

    Feat. - Implemented `Save()` and `Delete()` methods to `LanguageService`.

--- src/utils/renderer/services/LanguageService.ts
+++ src/utils/renderer/services/LanguageService.ts
@@ -10,4 +10,15 @@ export class LanguageService {
         const rawLanguage: Language = await window.txnmAPI.repositories.language.ReadOne(languageId);
         return Language.Hydrate(rawLanguage);
     }
+
+    public static async Save(language: Language): Promise<[boolean, Language | undefined]> {
+        let [success, savedLanguage] = language.GetId() == 0
+            ? await window.txnmAPI.repositories.language.Create(language)
+            : await window.txnmAPI.repositories.language.Update(language);
+        return [success, Language.Hydrate(savedLanguage)];
+    }
+
+    public static async Delete(language: Language) {
+        return await window.txnmAPI.repositories.language.Delete(language);
+    }
 }
