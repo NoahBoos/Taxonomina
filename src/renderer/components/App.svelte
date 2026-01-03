b183a4e09e5fd80ab2ec23f6a77840c100a7a6a3
Noah Boos <nboosgamemaker@gmail.com>
2026-01-03 19:57:16 +0100

    Feat. - Created the `Browser.svelte` component.

--- src/renderer/components/App.svelte
+++ src/renderer/components/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Navigator from "@/renderer/components/layouts/Navigator.svelte";
+    import Browser from "@/renderer/components/layouts/Browser.svelte";
 </script>
 
 <style>
@@ -8,4 +9,5 @@
 
 <div>
     <Navigator />
+    <Browser />
 </div>
