fc1b73ca577fdf87ac5345cbd6cfad1ba2efc923
Noah Boos <nboosgamemaker@gmail.com>
2026-01-03 03:26:16 +0100

    Feat. - Created and mounted the `App.svelte` component.

---
+++ src/renderer/components/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <h1>Hello World !</h1>
+</div>
--- src/renderer/pages/index/index.renderer.ts
+++ src/renderer/pages/index/index.renderer.ts
@@ -1,46 +1,57 @@
-import {LanguageUIBuilder} from "../../builders/LanguageUIBuilder";
-import {DictionaryUIBuilder} from "../../builders/DictionaryUIBuilder";
-import {GrammaticalClassUIBuilder} from "../../builders/GrammaticalClassUIBuilder";
-import {GrammaticalGenreUIBuilder} from "../../builders/GrammaticalGenreUIBuilder";
-import {EntryUIBuilder} from "../../builders/EntryUIBuilder";
-import {SettingUIBuilder} from "../../builders/SettingUIBuilder";
-import {TaxonominaSettings} from "../../../shared/interfaces/I_TaxonominaSettings";
+// import {LanguageUIBuilder} from "../../builders/LanguageUIBuilder";
+// import {DictionaryUIBuilder} from "../../builders/DictionaryUIBuilder";
+// import {GrammaticalClassUIBuilder} from "../../builders/GrammaticalClassUIBuilder";
+// import {GrammaticalGenreUIBuilder} from "../../builders/GrammaticalGenreUIBuilder";
+// import {EntryUIBuilder} from "../../builders/EntryUIBuilder";
+// import {SettingUIBuilder} from "../../builders/SettingUIBuilder";
+// import {TaxonominaSettings} from "../../../shared/interfaces/I_TaxonominaSettings";
+import {mount} from "svelte";
+import App from "@/renderer/components/App.svelte";
 
-export let settings: TaxonominaSettings;
+// export let settings: TaxonominaSettings;
 
 async function IndexRenderer() {
-    settings = await window.txnmAPI.settings.Load();
-    await InitializeTheme();
+    const app_container = document.getElementById("app");
+    if (!app_container) {
+        throw new Error("The App container (#app) is missing from the DOM.");
+    }
 
-    await DictionaryUIBuilder.Initialize();
+    const app = mount(App, { target: app_container });
 
-    await LanguageUIBuilder.Initialize();
-    await GrammaticalClassUIBuilder.Initialize();
-    await GrammaticalGenreUIBuilder.Initialize();
-    await EntryUIBuilder.Initialize();
-
-    await SettingUIBuilder.Initialize();
+    // settings = await window.txnmAPI.settings.Load();
+    // await InitializeTheme();
+    //
+    // await DictionaryUIBuilder.Initialize();
+    //
+    // await LanguageUIBuilder.Initialize();
+    // await GrammaticalClassUIBuilder.Initialize();
+    // await GrammaticalGenreUIBuilder.Initialize();
+    // await EntryUIBuilder.Initialize();
+    //
+    // await SettingUIBuilder.Initialize();
 }
 
-async function InitializeTheme() {
-    const settings: TaxonominaSettings = await window.txnmAPI.settings.Expose();
-    document.body.setAttribute("data-theme", settings.selectedTheme);
-    document.body.setAttribute("data-variant", settings.themeVariant);
-    document.body.setAttribute("data-font-size", settings.fontSize);
-    document.body.classList.toggle("hide-scrollbar", !settings.scrollbarVisibility);
-    document.querySelector("#help-window-button")!.classList.toggle("inactive", !settings.helpButtonVisibility);
-    EntryUIBuilder.pageSize = settings.elementsPerPage;
-    GrammaticalClassUIBuilder.pageSize = settings.elementsPerPage;
-    GrammaticalGenreUIBuilder.pageSize = settings.elementsPerPage;
-    LanguageUIBuilder.pageSize = settings.elementsPerPage;
-}
+// async function InitializeTheme() {
+//     const settings: TaxonominaSettings = await window.txnmAPI.settings.Expose();
+//     document.body.setAttribute("data-theme", settings.selectedTheme);
+//     document.body.setAttribute("data-variant", settings.themeVariant);
+//     document.body.setAttribute("data-font-size", settings.fontSize);
+//     document.body.classList.toggle("hide-scrollbar", !settings.scrollbarVisibility);
+//     document.querySelector("#help-window-button")!.classList.toggle("inactive", !settings.helpButtonVisibility);
+//     EntryUIBuilder.pageSize = settings.elementsPerPage;
+//     GrammaticalClassUIBuilder.pageSize = settings.elementsPerPage;
+//     GrammaticalGenreUIBuilder.pageSize = settings.elementsPerPage;
+//     LanguageUIBuilder.pageSize = settings.elementsPerPage;
+// }
 
-export function GetSettings() {
-    return settings;
-}
+// export function GetSettings() {
+//     return settings;
+// }
 
-export function SetSettings(newSettings: TaxonominaSettings) {
-    settings = newSettings;
-}
+// export function SetSettings(newSettings: TaxonominaSettings) {
+//     settings = newSettings;
+// }
 
-addEventListener("DOMContentLoaded", IndexRenderer);
+addEventListener("DOMContentLoaded", () => {
+    IndexRenderer().catch(console.error);
+});
