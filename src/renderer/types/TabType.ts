3531a0ae0efe2a6b567bef2f4e6f1845bf0511ec
Noah Boos <nboosgamemaker@gmail.com>
2026-01-04 00:34:11 +0100

    Feat. - Added `renderer/types/TabType`.

---
+++ src/renderer/types/TabType.ts
@@ -0,0 +1,4 @@
+import {ContentTab} from "@/renderer/enums/ContentTab";
+import {SpecialContentTab} from "@/renderer/enums/SpecialContentTab";
+
+export type TabType = ContentTab | SpecialContentTab;
