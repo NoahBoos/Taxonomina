30b5f434ede693cbd70236beafc6072abe230395
Noah Boos <nboosgamemaker@gmail.com>
2026-01-03 01:06:41 +0100

    Refacto. - Rewrote the configuration files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import path from "node:path";
 
 export default defineConfig({
-    root: path.resolve(__dirname, "src"), // dossier contenant index.html, preload.ts, renderer.ts
-    base: "./", // chemins relatifs corrects pour Electron
+    root: path.resolve(__dirname, "src/renderer"),
+    base: "./",
     build: {
-        outDir: path.resolve(__dirname, "dist/views/pages/index/renderer"), // output final
+        outDir: path.resolve(__dirname, "dist/renderer"),
         emptyOutDir: true,
-        target: "esnext", // moderne pour Electron
-        minify: false, // pratique pour debug
+        target: "esnext",
+        minify: false,
         rollupOptions: {
-            input: path.resolve(__dirname, "src/views/pages/index/renderer.ts"), // uniquement ton renderer
-            output: {
-                format: "es", // ES Module pour navigateur
-                entryFileNames: "renderer.js", // nom du bundle final
-            }
+            input: path.resolve(__dirname, "src/renderer/pages/index/index.html")
         }
     },
     resolve: {
