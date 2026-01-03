81f252a80bb3177dac4fd5fab51385b29be93d54
Noah Boos <nboosgamemaker@gmail.com>
2026-01-03 03:25:48 +0100

    Feat. - Added the Svelte plugin to `vite.config.ts`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import path from "node:path";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
     root: path.resolve(__dirname, "src/renderer"),
     base: "./",
+    plugins: [
+        svelte()
+    ],
     build: {
         outDir: path.resolve(__dirname, "dist/renderer"),
         emptyOutDir: true,
