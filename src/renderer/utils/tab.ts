0d95ac18bbc1e24412dc2303889a2dfbf50ce820
Noah Boos <nboosgamemaker@gmail.com>
2026-01-04 15:04:40 +0100

    Feat. - Added `getTabIcon()` function in `renderer/utils/tab.ts`.

--- src/renderer/utils/tab.ts
+++ src/renderer/utils/tab.ts
@@ -1,6 +1,7 @@
 import {TabType} from "@/renderer/types/TabType";
 import {ContentTab} from "@/renderer/enums/ContentTab";
 import {SpecialContentTab} from "@/renderer/enums/SpecialContentTab";
+import {Component} from "svelte";
 
 export function getTabLabel(tab: TabType): string {
     const contentTab: ContentTab | undefined = ContentTab.all!.find?.((loopedTab) => loopedTab === tab);
@@ -10,4 +11,14 @@ export function getTabLabel(tab: TabType): string {
     if (specialContentTab) return SpecialContentTab.labels[specialContentTab];
 
     return tab.toString();
+}
+
+export function getTabIcon(tab: TabType): Component | undefined {
+    const contentTab: ContentTab | undefined = ContentTab.all!.find?.((loopedTab) => loopedTab === tab);
+    if (contentTab) return ContentTab.icons[contentTab];
+
+    const specialContentTab: SpecialContentTab | undefined = SpecialContentTab.all!.find?.((loopedTab) => loopedTab === tab);
+    if (specialContentTab) return SpecialContentTab.icons[specialContentTab];
+
+    return undefined;
 }
