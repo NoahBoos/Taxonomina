c1621a7e8ad3ea0786d67adb394136273852d5a7
Noah Boos <nboosgamemaker@gmail.com>
2026-01-04 15:05:50 +0100

    Refacto. - `getTabLabel()` can now return an `undefined` value, if no label has been found.

--- src/renderer/utils/tab.ts
+++ src/renderer/utils/tab.ts
@@ -3,14 +3,14 @@ import {ContentTab} from "@/renderer/enums/ContentTab";
 import {SpecialContentTab} from "@/renderer/enums/SpecialContentTab";
 import {Component} from "svelte";
 
-export function getTabLabel(tab: TabType): string {
+export function getTabLabel(tab: TabType): string | undefined {
     const contentTab: ContentTab | undefined = ContentTab.all!.find?.((loopedTab) => loopedTab === tab);
     if (contentTab) return ContentTab.labels[contentTab];
 
     const specialContentTab: SpecialContentTab | undefined = SpecialContentTab.all!.find?.((loopedTab) => loopedTab === tab);
     if (specialContentTab) return SpecialContentTab.labels[specialContentTab];
 
-    return tab.toString();
+    return undefined;
 }
 
 export function getTabIcon(tab: TabType): Component | undefined {
