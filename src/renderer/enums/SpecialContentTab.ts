1e5fe58e014ce06a1d13310bfc8d200d0821a43c
Noah Boos <nboosgamemaker@gmail.com>
2026-01-04 02:00:54 +0100

    Feat. - Added namespace featuring access to the enum and its labels to `renderer/enums/SpecialContentTab`.

--- src/renderer/enums/SpecialContentTab.ts
+++ src/renderer/enums/SpecialContentTab.ts
@@ -2,3 +2,15 @@ export enum SpecialContentTab {
     Help,
     Settings
 }
+
+export namespace SpecialContentTab {
+    export const all: SpecialContentTab[] = [
+        SpecialContentTab.Help,
+        SpecialContentTab.Settings
+    ]
+
+    export const labels: Record<SpecialContentTab, string> = {
+        [SpecialContentTab.Help]: 'Aide',
+        [SpecialContentTab.Settings]: 'Paramètres',
+    }
+}
