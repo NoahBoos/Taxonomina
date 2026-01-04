9694a4ba9dec10e0f2389b019fa26187c3a04b02
Noah Boos <nboosgamemaker@gmail.com>
2026-01-04 19:16:11 +0100

    Feat. - Created the `AddContentButton` component.

---
+++ src/renderer/components/browser/AddContentButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {Plus} from "@lucide/svelte";
+
+    export let onClick: () => void;
+</script>
+
+<style>
+
+</style>
+
+<button on:click={onClick}>
+    <Plus />
+</button>
