31e5792773ab70eef59edc3c035beca4cf574624
Noah Boos <nboosgamemaker@gmail.com>
2026-01-04 18:42:11 +0100

    Feat. - Implemented the browser component refreshing, depending on `currentTabStore`.

--- src/renderer/components/layouts/Browser.svelte
+++ src/renderer/components/layouts/Browser.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {currentTabStore} from "@/renderer/stores/currentTabStore";
+    import {ContentTab} from "@/renderer/enums/ContentTab";
 </script>
 
 <style>
@@ -9,4 +10,9 @@
 <div>
     <p>This is the browser.</p>
     <p>The current tab is { $currentTabStore }</p>
+    {#each ContentTab.all as tab}
+        {#if tab === $currentTabStore}
+            <svelte:component this={ ContentTab.browsers[tab] } />
+        {/if}
+    {/each}
 </div>
