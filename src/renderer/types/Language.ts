bfe17f9a796591f0c6639da805d19b8cb9b7f73e
Noah Boos <nboosgamemaker@gmail.com>
2026-01-05 16:24:01 +0100

    Feat. - Added `renderer/types/Language`.

---
+++ src/renderer/types/Language.ts
@@ -0,0 +1,10 @@
+export interface Language {
+    id: number;
+    dictionary_id: number;
+    iso_639_1: string;
+    iso_639_3: string;
+    is_conlang: boolean;
+    name_native: string;
+    name_local: string;
+    direction: string;
+}
