b3a7a5c5e5993b3bbbccdacb2860b64f21cc2894
Noah Boos <nboosgamemaker@gmail.com>
2026-01-05 16:36:58 +0100

    Feat. - Added `GrammaticalGenreThumbnail` component.

---
+++ src/renderer/components/features/grammatical_genre/GrammaticalGenreThumbnail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {GrammaticalGenre} from "@/renderer/types/GrammaticalGenre";
+
+    export let grammatical_genre: GrammaticalGenre;
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <button>{ grammatical_genre.name }</button>
+</div>
