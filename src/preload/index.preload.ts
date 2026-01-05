5623fadbf53e01a643e5b5179931d1ae97adfd13
Noah Boos <nboosgamemaker@gmail.com>
2026-01-06 00:08:24 +0100

    Refacto. - Fully changed parameter and return types in `txnmAPI` declared in `index.preload.ts` and `global.d.ts`.

--- src/preload/index.preload.ts
+++ src/preload/index.preload.ts
@@ -1,11 +1,11 @@
 import {contextBridge, ipcRenderer} from "electron";
-import {Language} from "../shared/models/Language";
+import {I_Language} from "../shared/interfaces/I_Language";
 import {I_TaxonominaSettings} from "../shared/interfaces/I_TaxonominaSettings";
-import {Dictionary} from "../shared/models/Dictionary";
-import {GrammaticalClass} from "../shared/models/GrammaticalClass";
-import {GrammaticalGenre} from "../shared/models/GrammaticalGenre";
-import {Definition} from "../shared/models/Definition";
-import {Entry} from "../shared/models/Entry";
+import {I_Dictionary} from "../shared/interfaces/I_Dictionary";
+import {I_GrammaticalClass} from "../shared/interfaces/I_GrammaticalClass";
+import {I_GrammaticalGenre} from "../shared/interfaces/I_GrammaticalGenre";
+import {I_Definition} from "../shared/interfaces/I_Definition";
+import {I_Entry} from "../shared/interfaces/I_Entry";
 
 contextBridge.exposeInMainWorld("txnmAPI", {
     LoadTemplateAsString: async (templatePath: string) => ipcRenderer.invoke("txnmAPI:loadTemplateAsString", templatePath),
@@ -22,60 +22,60 @@ contextBridge.exposeInMainWorld("txnmAPI", {
     },
     repositories: {
         definition: {
-            ReadAll: (): Promise<Definition[]> => ipcRenderer.invoke("txnmAPI:repositories:definition:readAll"),
-            ReadAllByEntry: (rawEntry: Entry): Promise<Definition[]> => ipcRenderer.invoke("txnmAPI:repositories:definition:readAllByEntry", rawEntry),
+            ReadAll: (): Promise<I_Definition[]> => ipcRenderer.invoke("txnmAPI:repositories:definition:readAll"),
+            ReadAllByEntry: (rawEntry: I_Entry): Promise<I_Definition[]> => ipcRenderer.invoke("txnmAPI:repositories:definition:readAllByEntry", rawEntry),
             ReadOne: (definitionId: number) => ipcRenderer.invoke("txnmAPI:repositories:definition:readOne", definitionId),
-            BindToTranslation: (rawDefinition: Definition, rawTranslation: Entry) => ipcRenderer.invoke("txnmAPI:repositories:definition:bindToTranslation", rawDefinition, rawTranslation),
-            UnbindFromTranslation: (rawDefinition: Definition, rawTranslation: Entry) => ipcRenderer.invoke("txnmAPI:repositories:definition:unbindFromTranslation", rawDefinition, rawTranslation),
-            Create: (rawDefinition: Definition): Promise<[boolean, Definition | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:definition:create", rawDefinition),
-            Update: (rawDefinition: Definition): Promise<[boolean, Definition | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:definition:update", rawDefinition),
-            Delete: (rawDefinition: Definition): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:definition:delete", rawDefinition),
+            BindToTranslation: (rawDefinition: I_Definition, rawTranslation: I_Entry) => ipcRenderer.invoke("txnmAPI:repositories:definition:bindToTranslation", rawDefinition, rawTranslation),
+            UnbindFromTranslation: (rawDefinition: I_Definition, rawTranslation: I_Entry) => ipcRenderer.invoke("txnmAPI:repositories:definition:unbindFromTranslation", rawDefinition, rawTranslation),
+            Create: (rawDefinition: I_Definition): Promise<[boolean, I_Definition | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:definition:create", rawDefinition),
+            Update: (rawDefinition: I_Definition): Promise<[boolean, I_Definition | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:definition:update", rawDefinition),
+            Delete: (rawDefinition: I_Definition): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:definition:delete", rawDefinition),
         },
         dictionary: {
-            ReadAll: (): Promise<Dictionary[]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:readAll"),
-            ReadAllButOne: (rawDictionary: Dictionary): Promise<Dictionary[]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:readAllButOne", rawDictionary),
+            ReadAll: (): Promise<I_Dictionary[]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:readAll"),
+            ReadAllButOne: (rawDictionary: I_Dictionary): Promise<I_Dictionary[]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:readAllButOne", rawDictionary),
             ReadOne: (dictionaryId: number) => ipcRenderer.invoke("txnmAPI:repositories:dictionary:readOne", dictionaryId),
-            Create: (rawDictionary: Dictionary): Promise<[boolean, Entry | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:create", rawDictionary),
-            Update: (rawDictionary: Dictionary): Promise<[boolean, Entry | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:update", rawDictionary),
-            Delete: (rawDictionary: Dictionary): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:delete", rawDictionary),
+            Create: (rawDictionary: I_Dictionary): Promise<[boolean, I_Dictionary | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:create", rawDictionary),
+            Update: (rawDictionary: I_Dictionary): Promise<[boolean, I_Dictionary | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:update", rawDictionary),
+            Delete: (rawDictionary: I_Dictionary): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:dictionary:delete", rawDictionary),
         },
         entry: {
-            ReadAll: (dictionary_id: number): Promise<Entry[]> => ipcRenderer.invoke("txnmAPI:repositories:entry:readAll", dictionary_id),
-            ReadAllByGlobalTranslation: (rawEntry: Entry): Promise<Entry[]> => ipcRenderer.invoke("txnmAPI:repositories:entry:readAllByGlobalTranslation", rawEntry),
-            ReadAllByLocalTranslation: (rawDefinition: Definition): Promise<Entry[]> => ipcRenderer.invoke("txnmAPI:repositories:entry:readAllByLocalTranslation", rawDefinition),
+            ReadAll: (dictionary_id: number): Promise<I_Entry[]> => ipcRenderer.invoke("txnmAPI:repositories:entry:readAll", dictionary_id),
+            ReadAllByGlobalTranslation: (rawEntry: I_Entry): Promise<I_Entry[]> => ipcRenderer.invoke("txnmAPI:repositories:entry:readAllByGlobalTranslation", rawEntry),
+            ReadAllByLocalTranslation: (rawDefinition: I_Definition): Promise<I_Entry[]> => ipcRenderer.invoke("txnmAPI:repositories:entry:readAllByLocalTranslation", rawDefinition),
             ReadOne: (entryId: number) => ipcRenderer.invoke("txnmAPI:repositories:entry:readOne", entryId),
-            BindToGrammaticalClass: (rawEntry: Entry, rawClass: GrammaticalClass) => ipcRenderer.invoke("txnmAPI:repositories:entry:bindToGrammaticalClass", rawEntry, rawClass),
-            UnbindFromGrammaticalClass: (rawEntry: Entry, rawClass: GrammaticalClass) => ipcRenderer.invoke("txnmAPI:repositories:entry:unbindFromGrammaticalClass", rawEntry, rawClass),
-            BindToGrammaticalGenre: (rawEntry: Entry, rawGenre: GrammaticalGenre) => ipcRenderer.invoke("txnmAPI:repositories:entry:bindToGrammaticalGenre", rawEntry, rawGenre),
-            UnbindFromGrammaticalGenre: (rawEntry: Entry, rawGenre: GrammaticalGenre) => ipcRenderer.invoke("txnmAPI:repositories:entry:unbindFromGrammaticalGenre", rawEntry, rawGenre),
-            BindToTranslation: (rawEntry: Entry, rawTranslation: Entry) => ipcRenderer.invoke("txnmAPI:repositories:entry:bindToTranslation", rawEntry, rawTranslation),
-            UnbindFromTranslation: (rawEntry: Entry, rawTranslation: Entry) => ipcRenderer.invoke("txnmAPI:repositories:entry:unbindFromTranslation", rawEntry, rawTranslation),
-            Create: (rawEntry: Entry): Promise<[boolean, Entry | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:entry:create", rawEntry),
-            Update: (rawEntry: Entry): Promise<[boolean, Entry | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:entry:update", rawEntry),
-            Delete: (rawEntry: Entry): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:entry:delete", rawEntry),
+            BindToGrammaticalClass: (rawEntry: I_Entry, rawClass: I_GrammaticalClass) => ipcRenderer.invoke("txnmAPI:repositories:entry:bindToGrammaticalClass", rawEntry, rawClass),
+            UnbindFromGrammaticalClass: (rawEntry: I_Entry, rawClass: I_GrammaticalClass) => ipcRenderer.invoke("txnmAPI:repositories:entry:unbindFromGrammaticalClass", rawEntry, rawClass),
+            BindToGrammaticalGenre: (rawEntry: I_Entry, rawGenre: I_GrammaticalGenre) => ipcRenderer.invoke("txnmAPI:repositories:entry:bindToGrammaticalGenre", rawEntry, rawGenre),
+            UnbindFromGrammaticalGenre: (rawEntry: I_Entry, rawGenre: I_GrammaticalGenre) => ipcRenderer.invoke("txnmAPI:repositories:entry:unbindFromGrammaticalGenre", rawEntry, rawGenre),
+            BindToTranslation: (rawEntry: I_Entry, rawTranslation: I_Entry) => ipcRenderer.invoke("txnmAPI:repositories:entry:bindToTranslation", rawEntry, rawTranslation),
+            UnbindFromTranslation: (rawEntry: I_Entry, rawTranslation: I_Entry) => ipcRenderer.invoke("txnmAPI:repositories:entry:unbindFromTranslation", rawEntry, rawTranslation),
+            Create: (rawEntry: I_Entry): Promise<[boolean, I_Entry | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:entry:create", rawEntry),
+            Update: (rawEntry: I_Entry): Promise<[boolean, I_Entry | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:entry:update", rawEntry),
+            Delete: (rawEntry: I_Entry): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:entry:delete", rawEntry),
         },
         grammaticalClass: {
-            ReadAll: (dictionary_id: number): Promise<GrammaticalClass[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:readAll", dictionary_id),
-            ReadAllByEntry: (rawEntry: Entry): Promise<GrammaticalClass[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:readAllByEntry", rawEntry),
+            ReadAll: (dictionary_id: number): Promise<I_GrammaticalClass[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:readAll", dictionary_id),
+            ReadAllByEntry: (rawEntry: I_Entry): Promise<I_GrammaticalClass[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:readAllByEntry", rawEntry),
             ReadOne: (grammaticalClassId: number) => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:readOne", grammaticalClassId),
-            Create: (rawGrammaticalClass: GrammaticalClass): Promise<[boolean, GrammaticalClass | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:create", rawGrammaticalClass),
-            Update: (rawGrammaticalClass: GrammaticalClass): Promise<[boolean, GrammaticalClass | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:update", rawGrammaticalClass),
-            Delete: (rawGrammaticalClass: GrammaticalClass): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:delete", rawGrammaticalClass),
+            Create: (rawGrammaticalClass: I_GrammaticalClass): Promise<[boolean, I_GrammaticalClass | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:create", rawGrammaticalClass),
+            Update: (rawGrammaticalClass: I_GrammaticalClass): Promise<[boolean, I_GrammaticalClass | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:update", rawGrammaticalClass),
+            Delete: (rawGrammaticalClass: I_GrammaticalClass): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalClass:delete", rawGrammaticalClass),
         },
         grammaticalGenre: {
-            ReadAll: (dictionary_id: number): Promise<GrammaticalGenre[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:readAll", dictionary_id),
-            ReadAllByEntry: (rawEntry: Entry): Promise<GrammaticalGenre[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:readAllByEntry", rawEntry),
+            ReadAll: (dictionary_id: number): Promise<I_GrammaticalGenre[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:readAll", dictionary_id),
+            ReadAllByEntry: (rawEntry: I_Entry): Promise<I_GrammaticalGenre[]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:readAllByEntry", rawEntry),
             ReadOne: (gramGenreId: number) => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:readOne", gramGenreId),
-            Create: (rawGramGenre: GrammaticalGenre): Promise<[boolean, GrammaticalGenre | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:create", rawGramGenre),
-            Update: (rawGramGenre: GrammaticalGenre): Promise<[boolean, GrammaticalGenre | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:update", rawGramGenre),
-            Delete: (rawGramGenre: GrammaticalGenre): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:delete", rawGramGenre),
+            Create: (rawGramGenre: I_GrammaticalGenre): Promise<[boolean, I_GrammaticalGenre | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:create", rawGramGenre),
+            Update: (rawGramGenre: I_GrammaticalGenre): Promise<[boolean, I_GrammaticalGenre | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:update", rawGramGenre),
+            Delete: (rawGramGenre: I_GrammaticalGenre): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:grammaticalGenre:delete", rawGramGenre),
         },
         language: {
-            ReadAll: (dictionary_id: number): Promise<Language[]> => ipcRenderer.invoke("txnmAPI:repositories:language:readAll", dictionary_id),
+            ReadAll: (dictionary_id: number): Promise<I_Language[]> => ipcRenderer.invoke("txnmAPI:repositories:language:readAll", dictionary_id),
             ReadOne: (languageId: number) => ipcRenderer.invoke("txnmAPI:repositories:language:readOne", languageId),
-            Create: (rawLanguage: Language): Promise<[boolean, GrammaticalGenre | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:language:create", rawLanguage),
-            Update: (rawLanguage: Language): Promise<[boolean, GrammaticalGenre | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:language:update", rawLanguage),
-            Delete: (rawLanguage: Language): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:language:delete", rawLanguage)
+            Create: (rawLanguage: I_Language): Promise<[boolean, I_Language | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:language:create", rawLanguage),
+            Update: (rawLanguage: I_Language): Promise<[boolean, I_Language | undefined]> => ipcRenderer.invoke("txnmAPI:repositories:language:update", rawLanguage),
+            Delete: (rawLanguage: I_Language): Promise<boolean> => ipcRenderer.invoke("txnmAPI:repositories:language:delete", rawLanguage)
         }
     },
 })
--- src/shared/types/global.d.ts
+++ src/shared/types/global.d.ts
@@ -1,10 +1,10 @@
-import {Language} from "../models/Language";
-import {Dictionary} from "../models/Dictionary";
+import {I_Language} from "../interfaces/I_Language";
+import {I_Dictionary} from "../interfaces/I_Dictionary";
 import {I_TaxonominaSettings} from "../interfaces/I_TaxonominaSettings";
-import {GrammaticalClass} from "../models/GrammaticalClass";
-import {GrammaticalGenre} from "../models/GrammaticalGenre";
-import {Definition} from "../models/Definition";
-import {Entry} from "../models/Entry";
+import {I_GrammaticalClass} from "../interfaces/I_GrammaticalClass";
+import {I_GrammaticalGenre} from "../interfaces/I_GrammaticalGenre";
+import {I_Definition} from "../interfaces/I_Definition";
+import {I_Entry} from "../interfaces/I_Entry";
 import {ipcRenderer} from "electron";
 
 export {};
@@ -26,60 +26,60 @@ declare global {
             }
             repositories: {
                 definition: {
-                    ReadAll: () => Promise<Definition[]>,
-                    ReadAllByEntry: (rawEntry: Entry) => Promise<Definition[]>,
-                    ReadOne: (definition) => Promise<Definition>,
-                    BindToTranslation: (rawDefinition: Definition, rawTranslation: Entry) => Promise<boolean>,
-                    UnbindFromTranslation: (rawDefinition: Definition, rawTranslation: Entry) => Promise<boolean>,
-                    Create: (rawDefinition: Definition) => Promise<[boolean, Definition | undefined]>,
-                    Update: (rawDefinition: Definition) => Promise<[boolean, Definition | undefined]>,
-                    Delete: (rawDefinition: Definition) => Promise<boolean>,
+                    ReadAll: () => Promise<I_Definition[]>,
+                    ReadAllByEntry: (rawEntry: Entry) => Promise<I_Definition[]>,
+                    ReadOne: (definitionId: number) => Promise<I_Definition>,
+                    BindToTranslation: (rawDefinition: I_Definition, rawTranslation: Entry) => Promise<boolean>,
+                    UnbindFromTranslation: (rawDefinition: I_Definition, rawTranslation: Entry) => Promise<boolean>,
+                    Create: (rawDefinition: I_Definition) => Promise<[boolean, I_Definition | undefined]>,
+                    Update: (rawDefinition: I_Definition) => Promise<[boolean, I_Definition | undefined]>,
+                    Delete: (rawDefinition: I_Definition) => Promise<boolean>,
                 },
                 dictionary: {
-                    ReadAll: () => Promise<Dictionary[]>,
-                    ReadAllButOne: (rawDictionary) => Promise<Dictionary[]>,
-                    ReadOne: (dictionaryId) => Promise<Dictionary>,
-                    Create: (rawDictionary: Dictionary) => Promise<[boolean, Entry | undefined]>,
-                    Update: (rawDictionary: Dictionary) => Promise<[boolean, Entry | undefined]>,
-                    Delete: (rawDictionary: Dictionary) => Promise<boolean>,
+                    ReadAll: () => Promise<I_Dictionary[]>,
+                    ReadAllButOne: (rawDictionary) => Promise<I_Dictionary[]>,
+                    ReadOne: (dictionaryId) => Promise<I_Dictionary>,
+                    Create: (rawDictionary: I_Dictionary) => Promise<[boolean, I_Dictionary | undefined]>,
+                    Update: (rawDictionary: I_Dictionary) => Promise<[boolean, I_Dictionary | undefined]>,
+                    Delete: (rawDictionary: I_Dictionary) => Promise<boolean>,
                 },
                 entry: {
-                    ReadAll: (dictionary_id: number) => Promise<Entry[]>,
-                    ReadAllByGlobalTranslation: (rawEntry: Entry) => Promise<Entry[]>,
-                    ReadAllByLocalTranslation: (rawDefinition: Definition) => Promise<Entry[]>,
-                    ReadOne: (entry) => Promise<Entry>,
-                    Create: (rawEntry: Entry) => Promise<[boolean, Entry | undefined]>,
-                    BindToGrammaticalClass: (rawEntry: Entry, rawClass: GrammaticalClass) => Promise<boolean>,
-                    UnbindFromGrammaticalClass: (rawEntry: Entry, rawClass: GrammaticalClass) => Promise<boolean>,
-                    BindToGrammaticalGenre: (rawEntry: Entry, rawGenre: GrammaticalGenre) => Promise<boolean>,
-                    UnbindFromGrammaticalGenre: (rawEntry: Entry, rawGenre: GrammaticalGenre) => Promise<boolean>,
-                    BindToTranslation: (rawEntry: Entry, rawTranslation: Entry) => Promise<boolean>,
-                    UnbindFromTranslation: (rawEntry: Entry, rawTranslation: Entry) => Promise<boolean>,
-                    Update: (rawEntry: Entry) => Promise<[boolean, Entry | undefined]>,
-                    Delete: (rawEntry: Entry) => Promise<boolean>,
+                    ReadAll: (dictionary_id: number) => Promise<I_Entry[]>,
+                    ReadAllByGlobalTranslation: (rawEntry: I_Entry) => Promise<I_Entry[]>,
+                    ReadAllByLocalTranslation: (rawDefinition: I_Definition) => Promise<I_Entry[]>,
+                    ReadOne: (entry) => Promise<I_Entry>,
+                    Create: (rawEntry: I_Entry) => Promise<[boolean, I_Entry | undefined]>,
+                    BindToGrammaticalClass: (rawEntry: I_Entry, rawClass: I_GrammaticalClass) => Promise<boolean>,
+                    UnbindFromGrammaticalClass: (rawEntry: I_Entry, rawClass: I_GrammaticalClass) => Promise<boolean>,
+                    BindToGrammaticalGenre: (rawEntry: I_Entry, rawGenre: GrammaticalGenre) => Promise<boolean>,
+                    UnbindFromGrammaticalGenre: (rawEntry: I_Entry, rawGenre: GrammaticalGenre) => Promise<boolean>,
+                    BindToTranslation: (rawEntry: I_Entry, rawTranslation: I_Entry) => Promise<boolean>,
+                    UnbindFromTranslation: (rawEntry: I_Entry, rawTranslation: I_Entry) => Promise<boolean>,
+                    Update: (rawEntry: I_Entry) => Promise<[boolean, I_Entry | undefined]>,
+                    Delete: (rawEntry: I_Entry) => Promise<boolean>,
                 },
                 grammaticalClass: {
-                    ReadAll: (dictionary_id: number) => Promise<GrammaticalClass[]>,
-                    ReadAllByEntry: (rawEntry: Entry) => Promise<GrammaticalClass[]>,
-                    ReadOne: (grammaticalCategoryId) => Promise<GrammaticalClass>,
-                    Create: (rawGramCat: GrammaticalClass) => Promise<[boolean, GrammaticalClass | undefined]>,
-                    Update: (rawGramCat: GrammaticalClass) => Promise<[boolean, GrammaticalClass | undefined]>,
-                    Delete: (rawGramCat: GrammaticalClass) => Promise<boolean>,
+                    ReadAll: (dictionary_id: number) => Promise<I_GrammaticalClass[]>,
+                    ReadAllByEntry: (rawEntry: I_Entry) => Promise<I_GrammaticalClass[]>,
+                    ReadOne: (grammaticalCategoryId) => Promise<I_GrammaticalClass>,
+                    Create: (rawGramCat: I_GrammaticalClass) => Promise<[boolean, I_GrammaticalClass | undefined]>,
+                    Update: (rawGramCat: I_GrammaticalClass) => Promise<[boolean, I_GrammaticalClass | undefined]>,
+                    Delete: (rawGramCat: I_GrammaticalClass) => Promise<boolean>,
                 },
                 grammaticalGenre: {
-                    ReadAll: (dictionary_id: number) => Promise<GrammaticalGenre[]>,
-                    ReadAllByEntry: (rawEntry: Entry) => Promise<GrammaticalGenre[]>,
-                    ReadOne: (gramGenreId) => Promise<GrammaticalGenre>,
-                    Create: (rawGramGenre: GrammaticalGenre) => Promise<[boolean, GrammaticalGenre | undefined]>,
-                    Update: (rawGramGenre: GrammaticalGenre) => Promise<[boolean, GrammaticalGenre | undefined]>,
-                    Delete: (rawGramGenre: GrammaticalGenre) => Promise<boolean>,
+                    ReadAll: (dictionary_id: number) => Promise<I_GrammaticalGenre[]>,
+                    ReadAllByEntry: (rawEntry: I_Entry) => Promise<I_GrammaticalGenre[]>,
+                    ReadOne: (gramGenreId) => Promise<I_GrammaticalGenre>,
+                    Create: (rawGramGenre: I_GrammaticalGenre) => Promise<[boolean, I_GrammaticalGenre | undefined]>,
+                    Update: (rawGramGenre: I_GrammaticalGenre) => Promise<[boolean, I_GrammaticalGenre | undefined]>,
+                    Delete: (rawGramGenre: I_GrammaticalGenre) => Promise<boolean>,
                 }
                 language: {
-                    ReadAll: (dictionary_id: number) => Promise<Language[]>,
-                    ReadOne: (languageId: number) => Promise<Language>,
-                    Create: (rawLanguage: Language) => Promise<[boolean, GrammaticalGenre | undefined]>,
-                    Update: (rawLanguage: Language) => Promise<[boolean, GrammaticalGenre | undefined]>,
-                    Delete: (rawLanguage: Language) => Promise<boolean>
+                    ReadAll: (dictionary_id: number) => Promise<I_Language[]>,
+                    ReadOne: (languageId: number) => Promise<I_Language>,
+                    Create: (rawLanguage: I_Language) => Promise<[boolean, I_Language | undefined]>,
+                    Update: (rawLanguage: I_Language) => Promise<[boolean, I_Language | undefined]>,
+                    Delete: (rawLanguage: I_Language) => Promise<boolean>
                 }
             }
         }
