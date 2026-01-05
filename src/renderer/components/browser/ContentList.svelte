b2444a66444386db0d4473f5e6b61c38af47ce55
Noah Boos <nboosgamemaker@gmail.com>
2026-01-05 20:14:44 +0100

    Feat. - Added `ContentList` component.

---
+++ src/renderer/components/browser/ContentList.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {ContentType} from "@/renderer/enums/ContentType";
+    import {getContext} from "svelte";
+    import LanguageThumbnail from "@/renderer/components/features/language/LanguageThumbnail.svelte";
+    import GrammaticalClassThumbnail from "@/renderer/components/features/grammatical_class/GrammaticalClassThumbnail.svelte";
+    import GrammaticalGenreThumbnail from "@/renderer/components/features/grammatical_genre/GrammaticalGenreThumbnail.svelte";
+    import EntryThumbnail from "@/renderer/components/features/entry/EntryThumbnail.svelte";
+    import {CONTENT_TYPE_KEY} from "@/renderer/utils/symbols";
+
+    export let items: any = [];
+
+    let contentType: ContentType = getContext<ContentType>(CONTENT_TYPE_KEY);
+</script>
+
+<style>
+
+</style>
+
+<div>
+    {#if contentType === ContentType.Language}
+        {#each items as item }
+            <LanguageThumbnail language={item} />
+        {/each}
+    {:else if contentType === ContentType.GrammaticalClass}
+        {#each items as item }
+            <GrammaticalClassThumbnail grammatical_class={item} />
+        {/each}
+    {:else if contentType === ContentType.GrammaticalGenre}
+        {#each items as item }
+            <GrammaticalGenreThumbnail grammatical_genre={item} />
+        {/each}
+    {:else if contentType === ContentType.Entry}
+        {#each items as item }
+            <EntryThumbnail entry={item} />
+        {/each}
+    {/if}
+</div>
