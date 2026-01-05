9ceec3237f27095666d684534fbedde0b999e2ff
Noah Boos <nboosgamemaker@gmail.com>
2026-01-05 16:25:11 +0100

    Feat. - Added `LanguageThumbnail` component.

---
+++ src/renderer/components/features/language/LanguageThumbnail.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import {Language} from "@/renderer/types/Language";
+
+    export let language: Language;
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <button>
+        <span>{ language.name_native }</span>
+        <span>{ language.name_local }</span>
+    </button>
+</div>
