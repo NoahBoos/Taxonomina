d7533f7e86d19877cfe5ba751caaff7fc9aa452f
Noah Boos <nboosgamemaker@gmail.com>
2026-01-05 16:36:53 +0100

    Feat. - Added `GrammaticalClassThumbnail` component.

---
+++ src/renderer/components/features/grammatical_class/GrammaticalClassThumbnail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {GrammaticalClass} from "@/renderer/types/GrammaticalClass";
+
+    export let grammatical_class: GrammaticalClass;
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <button>{ grammatical_class.name }</button>
+</div>
