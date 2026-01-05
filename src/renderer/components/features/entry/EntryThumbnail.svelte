36fe24470ab23f2fb7f9409e2fb438cbae554207
Noah Boos <nboosgamemaker@gmail.com>
2026-01-05 16:36:46 +0100

    Feat. - Added `EntryThumbnail` component.

---
+++ src/renderer/components/features/entry/EntryThumbnail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {Entry} from "@/renderer/types/Entry";
+
+    export let entry: Entry;
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <button>{ entry.lemma }</button>
+</div>
