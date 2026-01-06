154b3d3d3f0011ddd58f6a7743d2b07eec4f320d
Noah Boos <nboosgamemaker@gmail.com>
2026-01-06 19:46:38 +0100

    Refacto. - Simplified `refresh()` in `LanguageBrowser`.

--- src/renderer/components/features/language/LanguageBrowser.svelte
+++ src/renderer/components/features/language/LanguageBrowser.svelte
@@ -25,21 +25,8 @@
     let dictionary_id = $derived($settings?.currentDictionary);
 
     async function refresh() {
-        if (dictionary_id) {
-            languages = (await LanguageService.ReadAll(dictionary_id)).map(language => ({
-                id: language.GetId(),
-                dictionary_id: language.GetDictionaryId(),
-                iso_639_1: language.GetIso639_1(),
-                iso_639_3: language.GetIso639_3(),
-                is_conlang: language.GetIsConlang(),
-                name_native: language.GetNameNative(),
-                name_local: language.GetNameLocal(),
-                direction: language.GetDirection()
-            }));
-            console.log(languages);
-        } else {
-            languages = [];
-        }
+        if (dictionary_id) languages = (await LanguageService.ReadAll(dictionary_id)).map(language => language.ToJSON());
+        else languages = [];
     }
 
     $effect(() => { refresh(); });
