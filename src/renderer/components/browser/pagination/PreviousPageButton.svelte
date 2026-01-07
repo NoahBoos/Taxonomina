80c0a52979ac0db2e38f0e6dd2e9e01edeee4245
Noah Boos <nboosgamemaker@gmail.com>
2026-01-07 17:48:36 +0100

    Feat. - Added `PreviousPageButton` component using `PaginationUtils.previous` for navigation.

---
+++ src/renderer/components/browser/pagination/PreviousPageButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {ChevronLeft} from "@lucide/svelte";
+    import {PaginationUtils} from "@/renderer/utils/pagination";
+
+    let { currentPage = $bindable(1) } = $props();
+</script>
+
+<style>
+
+</style>
+
+<button onclick={() => currentPage = PaginationUtils.previous(currentPage)}>
+    <ChevronLeft />
+</button>
