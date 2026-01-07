2c023450155b421eb682d2871a2f72319e925a67
Noah Boos <nboosgamemaker@gmail.com>
2026-01-07 18:19:53 +0100

    Feat. - Added `setPage` method to `PaginationUtils` for setting the current page.

--- src/renderer/utils/pagination.ts
+++ src/renderer/utils/pagination.ts
@@ -1,4 +1,8 @@
 export class PaginationUtils {
+    public static setPage(currentPage: number, page: number): number {
+        return currentPage === page ? currentPage : page;
+    }
+
     public static previous(currentPage: number): number {
         return currentPage > 1 ? currentPage - 1 : 1;
     }
