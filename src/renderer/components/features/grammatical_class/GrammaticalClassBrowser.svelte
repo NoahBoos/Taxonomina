42fc657f06915f748d727478eefb52c183717634
Noah Boos <nboosgamemaker@gmail.com>
2026-01-07 22:04:27 +0100

    Refacto. - Reused `CONTENT_TYPE_KEY` from `symbols` module in `EntryBrowser`, `GrammaticalClassBrowser`, and `GrammaticalGenreBrowser`.

--- src/renderer/components/features/grammatical_class/GrammaticalClassBrowser.svelte
+++ src/renderer/components/features/grammatical_class/GrammaticalClassBrowser.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import {ContentType} from "@/renderer/enums/ContentType";
     import {setContext} from "svelte";
+    import {CONTENT_TYPE_KEY} from "@/renderer/utils/symbols";
 
     const contentType: ContentType = ContentType.GrammaticalClass;
-    const CONTENT_TYPE_KEY = Symbol('contentType');
     setContext(CONTENT_TYPE_KEY, contentType);
 </script>
 
