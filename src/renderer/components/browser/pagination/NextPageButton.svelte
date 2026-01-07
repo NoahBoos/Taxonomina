c3d4bd9a76a2d24596b14602030815a176bb08ca
Noah Boos <nboosgamemaker@gmail.com>
2026-01-07 17:53:23 +0100

    Feat. - Added `NextPageButton` component using `PaginationUtils.next` for navigation.

---
+++ src/renderer/components/browser/pagination/NextPageButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {ChevronRight} from "@lucide/svelte";
+    import {PaginationUtils} from "@/renderer/utils/pagination";
+
+    let { currentPage = $bindable(1), totalPages = $bindable(1) } = $props();
+</script>
+
+<style>
+
+</style>
+
+<button onclick={() => currentPage = PaginationUtils.next(currentPage, totalPages)}>
+    <ChevronRight />
+</button>
