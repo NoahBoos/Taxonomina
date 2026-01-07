c36d525576055e3364f6cea276f5c1ab4d5613fd
Noah Boos <nboosgamemaker@gmail.com>
2026-01-07 17:50:24 +0100

    Feat. - Added `PaginationInformation` component to display the current page and elements per page.

---
+++ src/renderer/components/browser/pagination/PaginationInformation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let { currentPage = $bindable(1), totalPages, elementsPerPage } = $props();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    {#if totalPages === 0}
+        <p>Aucun élément à afficher.</p>
+    {:else}
+        <p>{ currentPage } / { totalPages }</p>
+        <p>{ elementsPerPage } éléments affichés.</p>
+    {/if}
+</div>
