767368cafca8b02542c329b4e98d38a28b267797
Noah Boos <nboosgamemaker@gmail.com>
2026-01-08 17:07:46 +0100

    Feat. - Added dynamic component rendering to `Inspector` layout using `INSPECTOR_REGISTRY` and `currentInspectorStateStore`.

--- src/renderer/components/layouts/Inspector.svelte
+++ src/renderer/components/layouts/Inspector.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+    import {currentInspectorStateStore} from "@/renderer/stores/currentInspectorStateStore";
+    import {INSPECTOR_REGISTRY} from "@/renderer/utils/registries/inspectorRegistry";
 
+    $: InspectorComponent = (() => {
+        const state = $currentInspectorStateStore;
+
+        if (state.category === 'content') {
+            return INSPECTOR_REGISTRY.content[state.type]?.[state.action];
+        } else if (state.category === 'special-content') {
+            return INSPECTOR_REGISTRY['special-content'][state.tab];
+        } else {
+            return INSPECTOR_REGISTRY['idle'];
+        }
+    })();
 </script>
 
 <style>
@@ -8,4 +21,5 @@
 
 <div>
     <p>This is the inspector panel.</p>
+    <InspectorComponent />
 </div>
