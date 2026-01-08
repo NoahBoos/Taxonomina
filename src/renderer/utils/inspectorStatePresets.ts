6aa27d236680afc47697aa9c8c15de70a586df6c
Noah Boos <nboosgamemaker@gmail.com>
2026-01-08 16:09:12 +0100

    Feat. - Added inspector state presets for standardized and reusable state management.

---
+++ src/renderer/utils/inspectorStatePresets.ts
@@ -0,0 +1,98 @@
+import {SpecialContentType} from "@/renderer/enums/SpecialContentType";
+import {InspectorAction} from "@/renderer/enums/InspectorAction";
+import {ContentType} from "@/renderer/enums/ContentType";
+import {InspectorState} from "@/renderer/types/InspectorState";
+
+export const INSPECTOR_STATE_PRESETS = {
+    IDLE: {
+        category: 'idle'
+    } as const,
+    CONTENT: {
+        ENTRY: {
+            READ_ONE: (id: number): InspectorState => ({
+                category: 'content',
+                type: ContentType.Entry,
+                action: InspectorAction.READ_ONE,
+                id: id
+            }),
+            CREATE: {
+                category: 'content',
+                type: ContentType.Entry,
+                action: InspectorAction.CREATE
+            } as const,
+            UPDATE: (id: number): InspectorState =>( {
+                category: 'content',
+                type: ContentType.Entry,
+                action: InspectorAction.UPDATE,
+                id: 0
+            })
+        },
+        GRAMMATICAL_CLASS: {
+            READ_ONE: (id: number): InspectorState => ({
+                category: 'content',
+                type: ContentType.GrammaticalClass,
+                action: InspectorAction.READ_ONE,
+                id: id
+            }),
+            CREATE: {
+                category: 'content',
+                type: ContentType.GrammaticalClass,
+                action: InspectorAction.CREATE
+            } as const,
+            UPDATE: (id: number): InspectorState =>( {
+                category: 'content',
+                type: ContentType.GrammaticalClass,
+                action: InspectorAction.UPDATE,
+                id: 0
+            })
+        },
+        GRAMMATICAL_GENRE: {
+            READ_ONE: (id: number): InspectorState => ({
+                category: 'content',
+                type: ContentType.GrammaticalGenre,
+                action: InspectorAction.READ_ONE,
+                id: id
+            }),
+            CREATE: {
+                category: 'content',
+                type: ContentType.GrammaticalGenre,
+                action: InspectorAction.CREATE
+            } as const,
+            UPDATE: (id: number): InspectorState =>( {
+                category: 'content',
+                type: ContentType.GrammaticalGenre,
+                action: InspectorAction.UPDATE,
+                id: 0
+            })
+        },
+        LANGUAGE: {
+            READ_ONE: (id: number): InspectorState => ({
+                category: 'content',
+                type: ContentType.Language,
+                action: InspectorAction.READ_ONE,
+                id: id
+            }),
+            CREATE: {
+                category: 'content',
+                type: ContentType.Language,
+                action: InspectorAction.CREATE
+            } as const,
+            UPDATE: (id: number): InspectorState =>( {
+                category: 'content',
+                type: ContentType.Language,
+                action: InspectorAction.UPDATE,
+                id: 0
+            })
+        }
+    },
+    SPECIAL_CONTENT: {
+        HELP: {
+            category: 'special-content',
+            tab: SpecialContentType.Help
+        } as const,
+        SETTINGS: {
+            category: 'special-content',
+            tab: SpecialContentType.Settings
+        } as const
+    }
+};
