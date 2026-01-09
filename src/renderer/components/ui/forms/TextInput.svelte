7c8ec9891af5c83ec59aebf2baa826154f7fe43a
Noah Boos <nboosgamemaker@gmail.com>
2026-01-09 13:27:12 +0100

    Feat. - Added `TextInput` component for reusable form input fields.

---
+++ src/renderer/components/ui/forms/TextInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { name, label, placeholder, value = $bindable('') }: { name: string, label: string, placeholder: string, value: string | number } = $props();
+    let id = crypto.randomUUID();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <label for={ id }>{ label }</label>
+    <input type="text" { id } { name } { placeholder } bind:value={ value } />
+</div>
