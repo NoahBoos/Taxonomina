062fff0d43f8564fd6f99d065add244439397ea2
Noah Boos <nboosgamemaker@gmail.com>
2026-01-09 13:34:00 +0100

    Feat. - Added `Checkbox` component for reusable form checkbox fields.

---
+++ src/renderer/components/ui/forms/Checkbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { name, label, checked = $bindable(false)}: { name: string, label: string, checked?: boolean } = $props();
+    let id = crypto.randomUUID();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <label for={ id }>{ label }</label>
+    <input type="checkbox" { id } { name } bind:checked={ checked } />
+</div>
