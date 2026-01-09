5d97e2ae546854764fbee7659ea06011887be666
Noah Boos <nboosgamemaker@gmail.com>
2026-01-09 13:40:09 +0100

    Feat. - Added `TextArea` component for reusable multiline text input fields.

---
+++ src/renderer/components/ui/forms/TextArea.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { name, label, rows, placeholder, value = $bindable() }: { name: string, label: string, rows: number, placeholder: string, value: string } = $props();
+    let id = crypto.randomUUID();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <label for={ id }>{ label }</label>
+    <textarea { id } { name } { rows } { placeholder } bind:value={ value }></textarea>
+</div>
