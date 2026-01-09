c11f66952e41e712caca8bed7859a5eaa1ed8215
Noah Boos <nboosgamemaker@gmail.com>
2026-01-10 00:38:07 +0100

    Refacto. - Simplified `GrammaticalClassService` by replacing `GrammaticalClass` model with `I_GrammaticalClass` interface and removing unused methods.

--- src/renderer/services/GrammaticalClassService.ts
+++ src/renderer/services/GrammaticalClassService.ts
@@ -1,48 +1,44 @@
-import {GrammaticalClass} from "@/main/database/models/GrammaticalClass";
-import {Entry} from "@/main/database/models/Entry";
-import {I_TaxonominaSettings} from "../../shared/interfaces/I_TaxonominaSettings";
+import {I_GrammaticalClass} from "@/shared/interfaces/I_GrammaticalClass";
+import {I_Entry} from "@/shared/interfaces/I_Entry";
 
 export class GrammaticalClassService {
-    public static async ReadAll(dictionary_id: number): Promise<GrammaticalClass[]> {
-        const rawGramCats: GrammaticalClass[] = await window.txnmAPI.repositories.grammaticalClass.ReadAll(dictionary_id);
-        return rawGramCats.map((rawGramCat: GrammaticalClass): GrammaticalClass => GrammaticalClass.Hydrate(rawGramCat));
+    public static async ReadAll(dictionary_id: number): Promise<I_GrammaticalClass[]> {
+        return await window.txnmAPI.repositories.grammaticalClass.ReadAll(dictionary_id);
     }
 
-    public static async ReadAllByEntry(entry: Entry): Promise<GrammaticalClass[]> {
-        const rawGramCats: GrammaticalClass[] = await window.txnmAPI.repositories.grammaticalClass.ReadAllByEntry(entry);
-        return rawGramCats.map((rawGramCat: GrammaticalClass): GrammaticalClass => GrammaticalClass.Hydrate(rawGramCat));
+    public static async ReadAllByEntry(entry: I_Entry): Promise<I_GrammaticalClass[]> {
+        return await window.txnmAPI.repositories.grammaticalClass.ReadAllByEntry(entry);
     }
 
-    public static async ReadOne(gramCatId: number): Promise<GrammaticalClass> {
-        const rawGramCat: GrammaticalClass = await window.txnmAPI.repositories.grammaticalClass.ReadOne(gramCatId);
-        return GrammaticalClass.Hydrate(rawGramCat);
+    public static async ReadOne(gramCatId: number): Promise<I_GrammaticalClass> {
+        return await window.txnmAPI.repositories.grammaticalClass.ReadOne(gramCatId);
     }
 
-    public static async Save(gramCat: GrammaticalClass): Promise<[boolean, GrammaticalClass | undefined]> {
-        let [success, savedGramCat] = gramCat.GetId() == 0
+    public static async Save(gramCat: I_GrammaticalClass): Promise<[boolean, I_GrammaticalClass | undefined]> {
+        let [success, savedGramCat] = gramCat.id == 0
             ? await window.txnmAPI.repositories.grammaticalClass.Create(gramCat)
             : await window.txnmAPI.repositories.grammaticalClass.Update(gramCat);
-        return [success, GrammaticalClass.Hydrate(savedGramCat)];
+        return [success, savedGramCat];
     }
 
-    public static async Delete(gramCat: GrammaticalClass): Promise<boolean> {
+    public static async Delete(gramCat: I_GrammaticalClass): Promise<boolean> {
         return await window.txnmAPI.repositories.grammaticalClass.Delete(gramCat);
     }
 
-    public static async FilterBySearch(dictionary_id: number, query: string): Promise<GrammaticalClass[]> {
-        const grammaticalCategories: GrammaticalClass[] = await GrammaticalClassService.ReadAll(dictionary_id);
-        return grammaticalCategories.filter(gc => {
-            return gc.GetName().toLowerCase().includes(query.toLowerCase());
-        });
-    }
-
-    public static async ProcessForm(form: Element): Promise<[boolean, GrammaticalClass | undefined]> {
-        const settings: I_TaxonominaSettings = await window.txnmAPI.settings.Load();
-        const id: number = Number(form.querySelector<HTMLInputElement>("#id")!.value);
-        const name: string = form.querySelector<HTMLInputElement>("#name")!.value;
-        let grammaticalCategory: GrammaticalClass = new GrammaticalClass(id, settings.currentDictionary, name);
-        if (!grammaticalCategory.Validate()) return [false, undefined];
-        grammaticalCategory.Normalize();
-        return await GrammaticalClassService.Save(grammaticalCategory);
-    }
+    // public static async FilterBySearch(dictionary_id: number, query: string): Promise<GrammaticalClass[]> {
+    //     const grammaticalCategories: GrammaticalClass[] = await GrammaticalClassService.ReadAll(dictionary_id);
+    //     return grammaticalCategories.filter(gc => {
+    //         return gc.GetName().toLowerCase().includes(query.toLowerCase());
+    //     });
+    // }
+    //
+    // public static async ProcessForm(form: Element): Promise<[boolean, GrammaticalClass | undefined]> {
+    //     const settings: I_TaxonominaSettings = await window.txnmAPI.settings.Load();
+    //     const id: number = Number(form.querySelector<HTMLInputElement>("#id")!.value);
+    //     const name: string = form.querySelector<HTMLInputElement>("#name")!.value;
+    //     let grammaticalCategory: GrammaticalClass = new GrammaticalClass(id, settings.currentDictionary, name);
+    //     if (!grammaticalCategory.Validate()) return [false, undefined];
+    //     grammaticalCategory.Normalize();
+    //     return await GrammaticalClassService.Save(grammaticalCategory);
+    // }
 }
