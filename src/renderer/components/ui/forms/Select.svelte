6e3d38b1fc8c94138f9ec7900d3dfaf0dded80da
Noah Boos <nboosgamemaker@gmail.com>
2026-01-09 13:27:24 +0100

    Feat. - Added `Select` component for reusable dropdown fields and `SelectOptions` type for defining option collections.

---
+++ src/renderer/components/ui/forms/Select.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import {SelectOptions} from "@/renderer/types/SelectOptions";
+
+    let { label, options, value = $bindable('') }: { label: string, options: SelectOptions, value: string | number } = $props();
+    let id = crypto.randomUUID();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <label for={ id }>{ label }</label>
+    <select { id } bind:value={ value }>
+        {#each Object.entries(options) as [option_value, option_label]}
+            <option value={ option_value }>{ option_label }</option>
+        {/each}
+    </select>
+</div>
---
+++ src/renderer/types/SelectOptions.ts
@@ -0,0 +1,5 @@
+/**
+ * A collection of options for a select element.
+ * The key is the value of the option, the value is the label of the option.
+ */
+export type SelectOptions = Record<string | number, string>;
