b1dd14676743288420e85a37292422c7603e7626
Noah Boos <nboosgamemaker@gmail.com>
2026-01-10 18:46:12 +0100

    Feat. - Enhanced `EntryForm` to handle CREATE and UPDATE with `inspectorRegistry`; integrated `EntrySection`, dynamic submit button, and form submission logic.

--- src/renderer/components/features/entry/EntryForm.svelte
+++ src/renderer/components/features/entry/EntryForm.svelte
@@ -1,11 +1,46 @@
 <script lang="ts">
+    import {currentInspectorStateStore, setCurrentInspectorState} from "@/renderer/stores/currentInspectorStateStore";
+    import {settings} from "@/renderer/stores/settingsStore";
+    import SubmitButton from "@/renderer/components/ui/forms/SubmitButton.svelte";
+    import {I_Entry} from "@/shared/interfaces/I_Entry";
+    import {EntryService} from "@/renderer/services/EntryService";
+    import {INSPECTOR_STATE_PRESETS} from "@/renderer/utils/inspectorStatePresets";
+    import EntrySection from "@/renderer/components/features/entry/form/EntrySection.svelte";
 
+    const dictionary_id: number = $settings!.currentDictionary;
+    let entry: I_Entry = $state<I_Entry>({id: 0, dictionary_id: dictionary_id, language_id: 0, lemma: ''});
+
+    let submitButtonLabel: string = $derived(entry.id === 0 ? 'Créer' : 'Modifier');
+
+    async function loadEntry() {
+        let inspectorState = $currentInspectorStateStore;
+        if (inspectorState.category === "content" && inspectorState.id !== undefined) {
+            const entry_data = await EntryService.ReadOne(inspectorState.id);
+            if (entry_data) {
+                Object.assign(entry, entry_data);
+            }
+        } else {
+            entry = {id: 0, dictionary_id: dictionary_id, language_id: 0, lemma: ''};
+        }
+    }
+
+    async function onSubmit(event: Event) {
+        event.preventDefault();
+        let _entry = $state.snapshot(entry);
+
+        await EntryService.Save(_entry);
+
+        setCurrentInspectorState(INSPECTOR_STATE_PRESETS.IDLE);
+    }
+
+    $effect(() => { loadEntry() });
 </script>
 
 <style>
 
 </style>
 
-<form>
-
+<form onsubmit={ onSubmit }>
+    <EntrySection { dictionary_id } bind:entry />
+    <SubmitButton label={ submitButtonLabel } />
 </form>
