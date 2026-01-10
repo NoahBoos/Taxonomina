c7ae7ab00a29d6649cfd6e909356f1c1b1fe28e4
Noah Boos <nboosgamemaker@gmail.com>
2026-01-10 17:47:57 +0100

    Feat. - Integrated `EntrySection` component.

--- src/renderer/components/features/entry/form/EntrySection.svelte
+++ src/renderer/components/features/entry/form/EntrySection.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+    import {I_Entry} from "@/shared/interfaces/I_Entry";
+    import TextInput from "@/renderer/components/ui/forms/TextInput.svelte";
+    import Select from "@/renderer/components/ui/forms/Select.svelte";
+    import {SelectOptions} from "@/renderer/types/SelectOptions";
+    import {LANGUAGES} from "@/renderer/utils/selectOptionPresets";
 
+    let { dictionary_id, entry = $bindable() }: { dictionary_id: number, entry: I_Entry } = $props();
+    let languageOptions = $state<SelectOptions>({});
+
+    $effect(() => { LANGUAGES(dictionary_id).then(options => languageOptions = options); })
 </script>
 
 <style>
@@ -7,5 +16,6 @@
 </style>
 
 <div>
-
+    <TextInput name="lemma" label="Lemme" placeholder="Chien, chat, pigeon..." bind:value={ entry.lemma } />
+    <Select label="Langue" options={ languageOptions } bind:value={ entry.language_id } />
 </div>
