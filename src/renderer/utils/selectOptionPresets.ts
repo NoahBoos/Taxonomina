2d2d829548143ff5163d35b257031723baa6c8f8
Noah Boos <nboosgamemaker@gmail.com>
2026-01-10 02:00:47 +0100

    Feat. - Added `LANGUAGES` preset to generate dynamic select options based on dictionary languages retrieved from `LanguageService`.

--- src/renderer/utils/selectOptionPresets.ts
+++ src/renderer/utils/selectOptionPresets.ts
@@ -1,6 +1,19 @@
 import {SelectOptions} from "@/renderer/types/SelectOptions";
+import {LanguageService} from "@/renderer/services/LanguageService";
+import {I_Language} from "@/shared/interfaces/I_Language";
 
 export const DIRECTION: SelectOptions = {
     'ltr': "Gauche à droite",
     'rtl': "Droite à gauche"
+}
+
+export const LANGUAGES = async (dictionaryId: number): Promise<SelectOptions> => {
+    const languages: I_Language[] = await LanguageService.ReadAll(dictionaryId);
+    const options: SelectOptions = {};
+
+    languages.forEach((language: I_Language) => {
+        options[language.id] = `${language.name_native} / ${language.name_local}`
+    });
+
+    return options;
 }
