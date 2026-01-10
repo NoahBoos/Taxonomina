b76cd92257cb782e8034244d49ea3d5dc25e3cb5
Noah Boos <nboosgamemaker@gmail.com>
2026-01-10 22:36:52 +0100

    Feat. - Enhanced `EntryForm` with grammatical class binding; integrated `GrammaticalClassSection`, updated submission logic, and improved entry loading.

--- src/renderer/components/features/entry/EntryForm.svelte
+++ src/renderer/components/features/entry/EntryForm.svelte
@@ -6,6 +6,9 @@
     import {EntryService} from "@/renderer/services/EntryService";
     import {INSPECTOR_STATE_PRESETS} from "@/renderer/utils/inspectorStatePresets";
     import EntrySection from "@/renderer/components/features/entry/form/EntrySection.svelte";
+    import GrammaticalClassSection from "@/renderer/components/features/entry/form/GrammaticalClassSection.svelte";
+    import {I_GrammaticalClass} from "@/shared/interfaces/I_GrammaticalClass";
+    import {GrammaticalClassService} from "@/renderer/services/GrammaticalClassService";
 
     const dictionary_id: number = $settings!.currentDictionary;
 
@@ -15,6 +18,7 @@
         language_id: 0,
         lemma: ''
     });
+    let selected_grammatical_classes = $state<I_GrammaticalClass[]>([]);
 
     let is_submitting: boolean = $state(false);
     let submit_button_label: string = $derived(entry.id === 0 ? 'Créer' : 'Modifier');
@@ -24,7 +28,10 @@
 
         if (inspectorState.category === "content" && inspectorState.id !== undefined) {
             const data = await EntryService.ReadOne(inspectorState.id);
-            if (data) Object.assign(entry, data);
+            if (data) {
+                Object.assign(entry, data);
+                selected_grammatical_classes = await GrammaticalClassService.ReadAllByEntry(data);
+            }
         } else {
             entry = { id: 0, dictionary_id: dictionary_id, language_id: 0, lemma: '' };
         }
@@ -37,7 +44,18 @@
 
         try {
             const entryToSave = $state.snapshot(entry);
-            await EntryService.Save(entryToSave);
+            const selectedGrammaticalClasses = $state.snapshot(selected_grammatical_classes);
+            const [success, savedEntry] = await EntryService.Save(entryToSave);
+            if (!success || !savedEntry) throw new Error("Failed to save the entry.")
+
+            if (entryToSave.id !== 0) {
+                let [old_grammatical_classes] = await Promise.all([
+                    await GrammaticalClassService.ReadAllByEntry(savedEntry)
+                ]);
+                old_grammatical_classes.forEach(gc => EntryService.UnbindFromGrammaticalClass(savedEntry, gc));
+            }
+
+            selectedGrammaticalClasses.forEach(gc => EntryService.BindToGrammaticalClass(savedEntry, gc));
 
             setCurrentInspectorState(INSPECTOR_STATE_PRESETS.IDLE);
         } catch (error) {
@@ -56,5 +74,6 @@
 
 <form onsubmit={ onSubmit }>
     <EntrySection { dictionary_id } bind:entry />
+    <GrammaticalClassSection { dictionary_id } bind:selected_grammatical_classes />
     <SubmitButton label={ submit_button_label } />
 </form>
