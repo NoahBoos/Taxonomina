a854f511cf3e879c0d8e137773a3abfa9f88d566
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 01:48:25 +0100

    Feat. - Added `Tag` component.

---
+++ src/renderer/components/ui/display/Tag.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import {X} from "@lucide/svelte";
+
+    interface Props {
+        label: string;
+        onRemove: (...args: any[]) => any;
+    }
+
+    let { label, onRemove }: Props = $props();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <p>{ label }</p>
+    <button type="button" onclick={ onRemove }>
+        <X />
+    </button>
+</div>
