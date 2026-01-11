1d7d4c14595425ce9afd618c9be9bd213758112d
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 13:29:43 +0100

    Feat. - Added `IconButton` component with icon support and click handler.

---
+++ src/renderer/components/ui/interactive/IconButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import {Component} from "svelte";
+
+    interface Props {
+        icon: Component;
+        onClick: () => void;
+    }
+
+    let { icon, onClick }: Props = $props();
+
+    let IconComponent = $derived.by(() => icon);
+</script>
+
+<style>
+
+</style>
+
+<button type="button" onclick={ onClick }>
+    <IconComponent />
+</button>
