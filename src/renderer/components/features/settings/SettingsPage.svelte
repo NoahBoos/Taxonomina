c0751bfc9e0c6516a1236a494a456d2f5627e004
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 18:19:53 +0100

    Feat. - Added scrollbar and help button visibility toggles to `SettingsPage`.

--- src/renderer/components/features/settings/SettingsPage.svelte
+++ src/renderer/components/features/settings/SettingsPage.svelte
@@ -2,11 +2,14 @@
     import Select from "@/renderer/components/ui/forms/Select.svelte";
     import {ELEMENTS_PER_PAGE, FONT_SIZES, THEME_VARIANTS, THEMES} from "@/renderer/utils/selectOptionPresets";
     import {settings, updateSetting} from "@/renderer/stores/settingsStore";
+    import Toggle from "@/renderer/components/ui/interactive/Toggle.svelte";
 
     let theme = $state($settings!.selectedTheme ?? 'default');
     let theme_variant = $state($settings!.themeVariant ?? 'light');
     let font_size = $state($settings!.fontSize ?? 'base');
     let elements_per_page = $state($settings!.elementsPerPage.toString() ?? '20');
+    let scrollbar_visibility = $state($settings!.scrollbarVisibility ?? true);
+    let help_button_visibility = $state($settings!.helpButtonVisibility ?? true);
 </script>
 
 <style>
@@ -24,7 +27,9 @@
         <div>
             <h3>Accessibilité</h3>
             <Select label="Taille du texte" options={ FONT_SIZES } bind:value={ font_size } onChange={ () => { updateSetting("fontSize", font_size) }} />
-            <Select label="Nombre d'éléments à afficher dans les navigateurs" options={ ELEMENTS_PER_PAGE } bind:value={ elements_per_page } onChange={ () => { updateSetting("elementsPerPage", elements_per_page) }} />
+            <Select label="Nombre d'éléments à afficher dans les navigateurs" options={ ELEMENTS_PER_PAGE } bind:value={ elements_per_page } onChange={ () => { updateSetting("elementsPerPage", Number(elements_per_page)) }} />
+            <Toggle name="scrollbar_visibility" label="Afficher les barres de défilement" bind:checked={ scrollbar_visibility } onChange={ () => { updateSetting("scrollbarVisibility", scrollbar_visibility) }} />
+            <Toggle name="help_button_visibility" label="Afficher le bouton d'aide" bind:checked={ help_button_visibility } onChange={ () => { updateSetting("helpButtonVisibility", help_button_visibility) }} />
         </div>
     </div>
 </div>
