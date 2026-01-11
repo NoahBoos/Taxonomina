0f7d20ef224bb7cb9693c63f5acd66e2d60957c0
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 01:07:58 +0100

    Feat. - Added `Searchbar` component with placeholder and query binding logic.

---
+++ src/renderer/components/ui/forms/Searchbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    interface Props {
+        placeholder: string;
+        query: string;
+    }
+
+    let { placeholder, query = $bindable('') }: Props = $props();
+    let id = crypto.randomUUID();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <input type="text" { id } { placeholder } bind:value={ query } />
+</div>
