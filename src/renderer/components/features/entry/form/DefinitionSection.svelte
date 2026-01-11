28a1bd4f5e971b36dceff077099b910b2419c2df
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 13:46:36 +0100

    Feat. - Added `DefinitionSection` functionality with definition management, including add/remove actions and input binding.

--- src/renderer/components/features/entry/form/DefinitionSection.svelte
+++ src/renderer/components/features/entry/form/DefinitionSection.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
+    import {I_Definition} from "@/shared/interfaces/I_Definition";
+    import IconButton from "@/renderer/components/ui/interactive/IconButton.svelte";
+    import {Minus, Plus} from "@lucide/svelte";
 
+    interface Props {
+        selected_definitions: I_Definition[];
+    }
+
+    let { selected_definitions = $bindable([]) }: Props = $props();
+
+    function addDefinition() {
+        let definition: I_Definition = { id: 0, definition: "" };
+        selected_definitions.push(definition);
+    }
+
+    function removeDefinition(definition: I_Definition) {
+        selected_definitions = selected_definitions.filter(d => d !== definition);
+    }
 </script>
 
 <style>
@@ -7,5 +24,11 @@
 </style>
 
 <div>
-
+    <IconButton icon={ Plus } onClick={ () => addDefinition() } />
+    {#each selected_definitions as definition}
+        <div>
+            <input type="text" bind:value={ definition.definition } />
+            <IconButton icon={ Minus } onClick={ () => removeDefinition(definition) } />
+        </div>
+    {/each}
 </div>
