84d485f811947c6e571a358382cd44d93455521e
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 18:19:46 +0100

    Feat. - Added `Toggle` component with props for controlled state and interactivity.

---
+++ src/renderer/components/ui/interactive/Toggle.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    interface Props {
+        name: string;
+        label: string;
+        checked?: boolean;
+        onChange?: (...args: any[]) => void;
+    }
+
+    let { name, label, checked = $bindable(false), onChange }: Props = $props();
+    let id = crypto.randomUUID();
+</script>
+
+<style>
+
+</style>
+
+<div>
+    <label for={ id }>{ label }</label>
+    <input type="checkbox" { id } { name } bind:checked={ checked } onchange={ onChange } />
+</div>
