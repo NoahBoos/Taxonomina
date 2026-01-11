9b13bd2819a80b1dd9107796f2952193f8e2df97
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 22:21:25 +0100

    Refactor. - Centralized tab click handling with dedicated functions and added calls to `resetCurrentInspectorState` and `resetCurrentBrowserTab` functions.

--- src/renderer/components/layouts/Navigator.svelte
+++ src/renderer/components/layouts/Navigator.svelte
@@ -2,9 +2,19 @@
     import {ContentType} from "@/renderer/enums/ContentType";
     import {SpecialContentType} from "@/renderer/enums/SpecialContentType";
     import NavigatorTabButton from "@/renderer/components/navigator/NavigatorTabButton.svelte";
-    import {updateCurrentBrowserTab} from "@/renderer/stores/currentBrowserTabStore";
-    import {setCurrentInspectorState} from "@/renderer/stores/currentInspectorStateStore";
+    import {currentBrowserTabStore, resetCurrentBrowserTab, updateCurrentBrowserTab} from "@/renderer/stores/currentBrowserTabStore";
+    import {resetCurrentInspectorState, setCurrentInspectorState} from "@/renderer/stores/currentInspectorStateStore";
     import {SPECIAL_CONTENT_INSPECTOR_STATES} from "@/renderer/utils/registries/inspectorRegistry";
+
+    function onContentTabClick(tab: ContentType) {
+        if (!ContentType.all.includes($currentBrowserTabStore as ContentType)) resetCurrentInspectorState();
+        updateCurrentBrowserTab(tab);
+    }
+
+    function onSpecialContentTabClick(tab: SpecialContentType) {
+        setCurrentInspectorState(SPECIAL_CONTENT_INSPECTOR_STATES[tab]);
+        resetCurrentBrowserTab();
+    }
 </script>
 
 <style>
@@ -14,12 +24,12 @@
 <div>
     <div>
         {#each ContentType.all as tab}
-            <NavigatorTabButton tab={ tab } onClick={ () => { updateCurrentBrowserTab(tab) }} />
+            <NavigatorTabButton tab={ tab } onClick={ () => { onContentTabClick(tab) }} />
         {/each}
     </div>
     <div>
         {#each SpecialContentType.all as tab}
-            <NavigatorTabButton tab={tab} onClick={ () => { setCurrentInspectorState(SPECIAL_CONTENT_INSPECTOR_STATES[tab]) }} />
+            <NavigatorTabButton tab={tab} onClick={ () => { onSpecialContentTabClick(tab) }} />
         {/each}
     </div>
 </div>
