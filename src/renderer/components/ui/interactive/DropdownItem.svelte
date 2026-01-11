562a437ea82095db022fb1c9c984424d46f866fb
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 01:10:42 +0100

    Feat. - Added `DropdownItem` component.

---
+++ src/renderer/components/ui/interactive/DropdownItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    interface Props {
+        label: string;
+        onClick: (...args: any[]) => void;
+    }
+
+    let { label, onClick }: Props = $props();
+</script>
+
+<style>
+
+</style>
+
+<li>
+    <button type="button" onclick={ onClick }>
+        { label }
+    </button>
+</li>
