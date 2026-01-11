989109e54b1ab070cbe899ad0688db8255ca6c1c
Noah Boos <nboosgamemaker@gmail.com>
2026-01-11 01:32:38 +0100

    Feat. - Added `Dropdown` component.

---
+++ src/renderer/components/ui/display/Dropdown.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    interface Props {
+        visible: boolean;
+        children: any;
+    }
+
+    let { visible, children }: Props = $props();
+</script>
+
+<style>
+
+</style>
+
+{#if visible}
+    <div>
+        <ul>
+            {@render children()}
+        </ul>
+    </div>
+{/if}
