4a316659bef7acda46022c76c920640df0a60782
Noah Boos <nboosgamemaker@gmail.com>
2026-01-14 12:31:59 +0100

    Feat. - Synchronized `<body>` theme attributes (`data-theme`, `data-theme-variant`) with `settingsStore` values.

--- src/renderer/components/App.svelte
+++ src/renderer/components/App.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+    import {settings} from "@/renderer/stores/settingsStore";
     import Navigator from "@/renderer/components/layouts/Navigator.svelte";
     import Inspector from "@/renderer/components/layouts/Inspector.svelte";
     import Browser from "@/renderer/components/layouts/Browser.svelte";
+
+    $effect(() => {
+        const theme = $settings!.selectedTheme ?? 'default';
+        const theme_variant = $settings!.themeVariant ?? 'light';
+
+        document.body.setAttribute('data-theme', theme);
+        document.body.setAttribute('data-theme-variant', theme_variant);
+    })
 </script>
 
 <style>
