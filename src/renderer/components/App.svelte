216688a44a92cf42548ac800b35e6cc0dd295406
Noah Boos <nboosgamemaker@gmail.com>
2026-01-14 22:59:31 +0100

    Feat. - Synchronized `<body>` attributes (`data-font-size`, `data-scrollbar-visibility`) with `settingsStore` values.

--- src/renderer/components/App.svelte
+++ src/renderer/components/App.svelte
@@ -7,10 +7,14 @@
     $effect(() => {
         const theme = $settings!.selectedTheme ?? 'default';
         const theme_variant = $settings!.themeVariant ?? 'light';
+        const font_size = $settings!.fontSize ?? 'base';
+        const scrollbar_visibility = $settings!.scrollbarVisibility ?? true;
 
         document.body.setAttribute('data-theme', theme);
         document.body.setAttribute('data-theme-variant', theme_variant);
-    })
+        document.body.setAttribute('data-font-size', font_size);
+        document.body.setAttribute('data-scrollbar-visibility', scrollbar_visibility.toString());
+    });
 </script>
 
 <style>
