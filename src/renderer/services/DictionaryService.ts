af8c0cdc70718e829ee42f0dba0d16156dde2fd7
Noah Boos <nboosgamemaker@gmail.com>
2026-01-15 20:53:27 +0100

    Refactor. - Replaced `Dictionary` model with `I_Dictionary` interface in `DictionaryService` methods for consistency and simplification.

--- src/renderer/services/DictionaryService.ts
+++ src/renderer/services/DictionaryService.ts
@@ -1,47 +1,44 @@
-import {Dictionary} from "@/main/database/models/Dictionary";
 import {I_TaxonominaSettings} from "../../shared/interfaces/I_TaxonominaSettings";
+import {I_Dictionary} from "@/shared/interfaces/I_Dictionary";
 // import {SetSettings} from "../pages/index/index.renderer";
 
 export class DictionaryService {
-    public static async GetCurrentDictionary(): Promise<Dictionary> {
+    public static async GetCurrentDictionary(): Promise<I_Dictionary> {
         let settings: I_TaxonominaSettings = await window.txnmAPI.settings.Expose();
-        let currentDictionary: Dictionary = await window.txnmAPI.repositories.dictionary.ReadOne(settings.currentDictionary);
-        return Dictionary.Hydrate(currentDictionary);
+        return await window.txnmAPI.repositories.dictionary.ReadOne(settings.currentDictionary);
     }
 
-    public static async SetCurrentDictionary(dictionary: Dictionary): Promise<void> {
-        await window.txnmAPI.settings.Update("currentDictionary", dictionary.GetId());
+    public static async SetCurrentDictionary(dictionary: I_Dictionary): Promise<void> {
+        await window.txnmAPI.settings.Update("currentDictionary", dictionary.id);
         // SetSettings(await window.txnmAPI.settings.Expose());
     }
 
-    public static async GetAllDictionaries(): Promise<Dictionary[]> {
-        const rawDictionaries = await window.txnmAPI.repositories.dictionary.ReadAll();
-        return rawDictionaries.map((dictionary: Dictionary): Dictionary => Dictionary.Hydrate(dictionary));
+    public static async GetAllDictionaries(): Promise<I_Dictionary[]> {
+        return await window.txnmAPI.repositories.dictionary.ReadAll();
     }
 
-    public static async GetAllDictionariesButOne(dictionaryToIgnore: Dictionary): Promise<Dictionary[]> {
-        const rawDictionaries = await window.txnmAPI.repositories.dictionary.ReadAllButOne(dictionaryToIgnore);
-        return rawDictionaries.map((dictionary: Dictionary): Dictionary => Dictionary.Hydrate(dictionary));
+    public static async GetAllDictionariesButOne(dictionaryToIgnore: I_Dictionary): Promise<I_Dictionary[]> {
+        return await window.txnmAPI.repositories.dictionary.ReadAllButOne(dictionaryToIgnore);
     }
 
-    public static async Delete(dictionary: Dictionary): Promise<boolean> {
+    public static async Delete(dictionary: I_Dictionary): Promise<boolean> {
         return await window.txnmAPI.repositories.dictionary.Delete(dictionary);
     }
 
-    public static async Save(dictionary: Dictionary): Promise<[boolean, Dictionary | undefined]> {
-        let [success, savedDictionary] = dictionary.GetId() == 0
+    public static async Save(dictionary: I_Dictionary): Promise<[boolean, I_Dictionary | undefined]> {
+        let [success, savedDictionary] = dictionary.id == 0
             ? await window.txnmAPI.repositories.dictionary.Create(dictionary)
             : await window.txnmAPI.repositories.dictionary.Update(dictionary);
-        return [success, Dictionary.Hydrate(savedDictionary)];
+        return [success, savedDictionary];
     }
 
-    public static async ProcessForm(form: Element): Promise<[boolean, Dictionary | undefined]> {
-        const id: number = Number(form.querySelector<HTMLInputElement>("#id")!.value);
-        const name: string = form.querySelector<HTMLInputElement>("#name")!.value;
-        const description: string = form.querySelector<HTMLInputElement>("#description")!.value;
-        let dictionary: Dictionary = new Dictionary(id, name, description);
-        if (!dictionary.Validate()) return [false, undefined];
-        dictionary.Normalize();
-        return await DictionaryService.Save(dictionary);
-    }
+    // public static async ProcessForm(form: Element): Promise<[boolean, I_Dictionary | undefined]> {
+    //     const id: number = Number(form.querySelector<HTMLInputElement>("#id")!.value);
+    //     const name: string = form.querySelector<HTMLInputElement>("#name")!.value;
+    //     const description: string = form.querySelector<HTMLInputElement>("#description")!.value;
+    //     let dictionary: I_Dictionary = new I_Dictionary(id, name, description);
+    //     if (!dictionary.Validate()) return [false, undefined];
+    //     dictionary.Normalize();
+    //     return await DictionaryService.Save(dictionary);
+    // }
 }
