ef2e2772087dba24be86b900f1a4cf66ccc2b6cc
Noah Boos <nboosgamemaker@gmail.com>
2026-01-16 17:20:02 +0100

    Feat. - Added style to the `DictionaryInformationButton` component.

--- src/renderer/components/features/dictionary/DictionaryInformationButton.svelte
+++ src/renderer/components/features/dictionary/DictionaryInformationButton.svelte
@@ -5,13 +5,31 @@
     onMount(() => { refreshCurrentDictionary() });
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    #dictionary-information-button {
+        @apply flex flex-col items-start gap-2 p-2 border-2 border-base-40 rounded-md bg-base-10 transition-colors duration-250 ease-out;
+    }
+
+    #dictionary-information-button:hover {
+        @apply text-base-90 bg-base-30 border-primary-400;
+
+        > .current-dictionary-name {
+            @apply border-primary-400;
+        }
+    }
+
+    .current-dictionary-name {
+        @apply text-base text-left pb-2 mb-2 border-b-2 border-base-40 w-full transition-colors duration-250 ease-out;
+    }
+
+    .current-dictionary-description {
+        @apply text-sm text-left w-full;
+    }
 </style>
 
-<div>
-    <button type="button">
-        <span>{ $currentDictionary?.name }</span>
-        <span>{ $currentDictionary?.description }</span>
-    </button>
-</div>
+<button type="button" id="dictionary-information-button">
+    <span class="current-dictionary-name">{ $currentDictionary?.name }</span>
+    <span class="current-dictionary-description">{ $currentDictionary?.description }</span>
+</button>
