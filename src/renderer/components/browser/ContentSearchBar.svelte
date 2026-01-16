63c8414d9850ab7cc4154087e2fdf4813deb22ca
Noah Boos <nboosgamemaker@gmail.com>
2026-01-16 22:41:50 +0100

    Feat. - Styled the ` ContentSearchBar ` component.

--- src/renderer/components/browser/ContentSearchBar.svelte
+++ src/renderer/components/browser/ContentSearchBar.svelte
@@ -10,14 +10,16 @@
     }
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../styles/styles.css';
 
+    .browser-searchbar {
+        @apply text-base-100 px-2 py-1 border-2 rounded-md border-primary-300 bg-transparent w-full h-fit transition-colors duration-250 ease-out;
+    }
 </style>
 
-<div>
-    {#each ContentType.all as tab}
-        {#if tab === $currentBrowserTabStore}
-            <input type="text" placeholder="{ ContentType.searchbarLabels[tab] }" bind:value={ query } oninput={ handleInput } />
-        {/if}
-    {/each}
-</div>
+{#each ContentType.all as tab}
+    {#if tab === $currentBrowserTabStore}
+        <input type="text" placeholder="{ ContentType.searchbarLabels[tab] }" bind:value={ query } oninput={ handleInput } class="browser-searchbar" />
+    {/if}
+{/each}
