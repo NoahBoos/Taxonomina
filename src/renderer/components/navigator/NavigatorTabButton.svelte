8eaf5c784561ec8b7982e937fffc8bb8643fab56
Noah Boos <nboosgamemaker@gmail.com>
2026-01-16 17:27:12 +0100

    Feat. - Added style to the `NavigatorTabButton` component.

--- src/renderer/components/navigator/NavigatorTabButton.svelte
+++ src/renderer/components/navigator/NavigatorTabButton.svelte
@@ -13,10 +13,18 @@
     let IconComponent = $derived.by(() => getTabIcon(tab));
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../styles/styles.css';
 
+    .tab-button {
+        @apply text-left text-base flex flex-row items-center gap-2 p-2 border-2 border-base-40 rounded-md bg-base-10 w-full transition-colors duration-250 ease-out;
+    }
+
+    .tab-button:hover {
+        @apply text-base-90 hover:bg-base-30 border-primary-400;
+    }
 </style>
 
-<button onclick={ onClick }>
+<button onclick={ onClick } class="tab-button">
     <IconComponent /> { label }
 </button>
