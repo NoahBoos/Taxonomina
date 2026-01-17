dfc103d9a33583e774d225f3f5d7c771e35aa1f7
Noah Boos <nboosgamemaker@gmail.com>
2026-01-17 18:58:26 +0100

    Feat. - Styled the `SubmitButton` component.

--- src/renderer/components/ui/forms/SubmitButton.svelte
+++ src/renderer/components/ui/forms/SubmitButton.svelte
@@ -2,8 +2,16 @@
     let { label }: { label: string } = $props();
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    button {
+        @apply block font-semibold px-4 py-2 mx-auto border-2 rounded-md border-base-40 bg-base-10 w-fit transition-colors duration-250 ease-out;
+    }
+
+    button:hover {
+        @apply border-primary-300 bg-base-20;
+    }
 </style>
 
 <button type="submit">
