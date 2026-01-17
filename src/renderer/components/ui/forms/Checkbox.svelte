2d5bd40f87fc23e1e76db3fdd328c6fa6b2dede0
Noah Boos <nboosgamemaker@gmail.com>
2026-01-17 20:57:00 +0100

    Feat. - Styled the `Checkbox` component. Added a `variant` value to its props to define its default or minimal appearance.

--- src/renderer/components/ui/forms/Checkbox.svelte
+++ src/renderer/components/ui/forms/Checkbox.svelte
@@ -4,17 +4,26 @@
         label: string;
         checked?: boolean;
         onChange?: (...args: any[]) => void;
+        variant?: 'default' | 'minimal';
     }
 
-    let { name, label, checked = $bindable(false), onChange }: Props = $props();
+    let { name, label, checked = $bindable(false), onChange, variant = 'default' }: Props = $props();
     let id = crypto.randomUUID();
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    input {
+        @apply m-0 border-2 bg-base-10 transform scale-100;
+    }
+
+    input:hover {
+        @apply border-primary-300 bg-base-20;
+    }
 </style>
 
-<div>
+<div class="{ variant === 'default' ? 'form-field-container' : '' } flex flex-row gap-2">
+    <input type="checkbox" { id } { name } bind:checked={ checked } onchange={ onChange } class="input-checkbox" />
     <label for={ id }>{ label }</label>
-    <input type="checkbox" { id } { name } bind:checked={ checked } onchange={ onChange } />
 </div>
