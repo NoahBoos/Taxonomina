374efaa73609d7323c901e7bd6c2e60e6275fc6a
Noah Boos <nboosgamemaker@gmail.com>
2026-01-17 20:58:39 +0100

    Feat. - Styled the `Select` component.

--- src/renderer/components/ui/forms/Select.svelte
+++ src/renderer/components/ui/forms/Select.svelte
@@ -12,11 +12,19 @@
     let id = crypto.randomUUID();
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    select {
+        @apply px-2 py-1 border-2 rounded-lg border-base-40 bg-base-10 w-full transition-colors duration-250 ease-out;
+    }
+
+    select:hover {
+        @apply border-primary-300 bg-base-20;
+    }
 </style>
 
-<div>
+<div class="form-field-container space-y-2">
     <label for={ id }>{ label }</label>
     <select { id } bind:value={ value } onchange={ onChange }>
         {#each Object.entries(options) as [option_value, option_label]}
