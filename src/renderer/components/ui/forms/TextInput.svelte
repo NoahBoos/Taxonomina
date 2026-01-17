e2ec92468deca72e46d64ceb360ae7ae0ce7be11
Noah Boos <nboosgamemaker@gmail.com>
2026-01-17 20:59:01 +0100

    Feat. - Styled the `TextInput` component.

--- src/renderer/components/ui/forms/TextInput.svelte
+++ src/renderer/components/ui/forms/TextInput.svelte
@@ -3,11 +3,19 @@
     let id = crypto.randomUUID();
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    input {
+        @apply px-2 py-1 border-2 rounded-lg border-base-40 bg-base-10 w-full transition-colors duration-250 ease-out;
+    }
+
+    input:hover {
+        @apply border-primary-300 bg-base-20;
+    }
 </style>
 
-<div>
+<div class="form-field-container space-y-2">
     <label for={ id }>{ label }</label>
     <input type="text" { id } { name } { placeholder } bind:value={ value } />
 </div>
