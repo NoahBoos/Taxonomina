bb87f6bf1853d99f1e64d7be2ac72ab194378214
Noah Boos <nboosgamemaker@gmail.com>
2026-01-18 00:05:40 +0100

    Feat. - Styled the `IconButton` component.

--- src/renderer/components/ui/interactive/IconButton.svelte
+++ src/renderer/components/ui/interactive/IconButton.svelte
@@ -11,8 +11,16 @@
     let IconComponent = $derived.by(() => icon);
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    button {
+        @apply p-1 border-2 rounded-md border-primary-300 w-fit transition-colors duration-250 ease-out;
+    }
+
+    button:hover {
+        @apply text-base-90 border-primary-400 bg-primary-500;
+    }
 </style>
 
 <button type="button" onclick={ onClick }>
