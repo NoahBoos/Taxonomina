5db25b63a5868c07176948a13c7a0f99a071238c
Noah Boos <nboosgamemaker@gmail.com>
2026-01-17 15:15:08 +0100

    Feat. - Added welcome message and updated `Idle` inspector styling for improved user guidance and layout alignment.

--- src/renderer/components/inspector/Idle.svelte
+++ src/renderer/components/inspector/Idle.svelte
@@ -6,6 +6,8 @@
 
 </style>
 
-<div>
+<div class="flex flex-col justify-center items-center h-full">
+    <h2>Bienvenue dans Taxonomina.</h2>
     <p>L'inspecteur n'affiche pour l'instant aucune vue.</p>
+    <p>Il se mettra à jour au fur et à mesure de vos interactions.</p>
 </div>
