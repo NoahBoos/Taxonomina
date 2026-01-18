38b2523587361a6387398493b8b56c4dd341ceb7
Noah Boos <nboosgamemaker@gmail.com>
2026-01-18 15:32:29 +0100

    Feat. - Styled the `Tag` component.

--- src/renderer/components/ui/display/Tag.svelte
+++ src/renderer/components/ui/display/Tag.svelte
@@ -9,13 +9,33 @@
     let { label, onRemove }: Props = $props();
 </script>
 
-<style>
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
 
+    div {
+        @apply flex flex-row items-center gap-2 px-2 py-1 border-2 rounded-lg border-base-40 bg-base-10 size-fit transition-colors duration-250 ease-out;
+    }
+
+    div:hover {
+        @apply border-primary-300 bg-base-20;
+
+        button {
+            @apply block;
+        }
+    }
+
+    div:not(:hover) button {
+        @apply hidden;
+    }
+
+    button {
+        @apply size-fit;
+    }
 </style>
 
 <div>
     <p>{ label }</p>
     <button type="button" onclick={ onRemove }>
-        <X />
+        <X size={ 16 } />
     </button>
 </div>
