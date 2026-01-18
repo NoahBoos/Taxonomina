69196742d392dde0ffb804c58ee714ee84507d37
Noah Boos <nboosgamemaker@gmail.com>
2026-01-18 18:08:30 +0100

    Feat. - Styled the `SettingsPage` component.

--- src/renderer/components/features/settings/SettingsPage.svelte
+++ src/renderer/components/features/settings/SettingsPage.svelte
@@ -16,18 +16,22 @@
 
 </style>
 
-<div>
-    <h3>Paramètres</h3>
-    <div>
-        <div>
-            <h3>Apparence</h3>
+<div class="space-y-4">
+    <h2>Paramètres</h2>
+    <div class="space-y-2">
+        <h3>Apparence</h3>
+        <div class="flex flex-row gap-2">
             <Select label="Thème" options={ THEMES } bind:value={ theme } onChange={ () => { updateSetting("selectedTheme", theme) }} />
             <Select label="Variante" options={ THEME_VARIANTS } bind:value={ theme_variant } onChange={ () => { updateSetting("themeVariant", theme_variant) }} />
         </div>
-        <div>
-            <h3>Accessibilité</h3>
+    </div>
+    <div class="space-y-2">
+        <h3>Accessibilité</h3>
+        <div class="flex flex-row gap-2">
             <Select label="Taille du texte" options={ FONT_SIZES } bind:value={ font_size } onChange={ () => { updateSetting("fontSize", font_size) }} />
             <Select label="Nombre d'éléments à afficher dans les navigateurs" options={ ELEMENTS_PER_PAGE } bind:value={ elements_per_page } onChange={ () => { updateSetting("elementsPerPage", Number(elements_per_page)) }} />
+        </div>
+        <div class="flex flex-row gap-2">
             <Toggle name="scrollbar_visibility" label="Afficher les barres de défilement" bind:checked={ scrollbar_visibility } onChange={ () => { updateSetting("scrollbarVisibility", scrollbar_visibility) }} />
             <Toggle name="help_button_visibility" label="Afficher le bouton d'aide" bind:checked={ help_button_visibility } onChange={ () => { updateSetting("helpButtonVisibility", help_button_visibility) }} />
         </div>
