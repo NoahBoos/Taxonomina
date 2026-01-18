46ff0deddd19835c2896f1d2924bf5689aef4741
Noah Boos <nboosgamemaker@gmail.com>
2026-01-18 18:15:12 +0100

    Feat. - Styled the `Toggle` component.

--- src/renderer/components/ui/interactive/Toggle.svelte
+++ src/renderer/components/ui/interactive/Toggle.svelte
@@ -11,10 +11,60 @@
 </script>
 
 <style>
+    input {
+        --toggle-size: 10px;
 
+        appearance: none;
+        width: 4.8em;
+        height: 2.7em;
+        font-size: var(--toggle-size);
+
+        background: linear-gradient(
+                to right,
+                var(--color-base-10) 50%,
+                var(--color-primary-400) 50%
+        ) no-repeat;
+        background-size: 205%;
+        background-position: 0;
+
+        transition: 0.4s;
+        border-radius: 99em;
+        position: relative;
+        cursor: pointer;
+    }
+
+    input::before {
+        content: "";
+        width: 1.8em;
+        height: 1.8em;
+
+        position: absolute;
+        top: 0.425em;
+        left: 0.35em;
+
+        background: linear-gradient(
+                to right,
+                var(--color-base-10) 50%,
+                var(--color-primary-400) 50%
+        ) no-repeat;
+        background-size: 205%;
+        background-position: 100%;
+
+        border-radius: 50%;
+        transition: 0.4s;
+    }
+
+    input:checked {
+        background-position: 100%;
+    }
+
+    input:checked::before {
+        left: calc(100% - 1.8em - 0.35em);
+        background-position: 0;
+    }
 </style>
 
-<div>
-    <label for={ id }>{ label }</label>
+<div class="form-field-container flex flex-row items-center gap-2">
     <input type="checkbox" { id } { name } bind:checked={ checked } onchange={ onChange } />
+    <label for={ id }>{ label }</label>
 </div>
