bb5acf328e330954f15e78a566c5d106fe70e556
Noah Boos <nboosgamemaker@gmail.com>
2026-01-26 00:13:37 +0100

    Refactor - Removed useless code from the `index.renderer.ts`.

--- src/renderer/pages/index/index.renderer.ts
+++ src/renderer/pages/index/index.renderer.ts
@@ -1,15 +1,6 @@
-// import {LanguageUIBuilder} from "../../builders/LanguageUIBuilder";
-// import {DictionaryUIBuilder} from "../../builders/DictionaryUIBuilder";
-// import {GrammaticalClassUIBuilder} from "../../builders/GrammaticalClassUIBuilder";
-// import {GrammaticalGenreUIBuilder} from "../../builders/GrammaticalGenreUIBuilder";
-// import {EntryUIBuilder} from "../../builders/EntryUIBuilder";
-// import {SettingUIBuilder} from "../../builders/SettingUIBuilder";
-// import {TaxonominaSettings} from "../../../shared/interfaces/I_TaxonominaSettings";
 import {mount} from "svelte";
 import App from "@/renderer/components/App.svelte";
 import {loadSettings} from "@/renderer/stores/settingsStore";
-
-// export let settings: TaxonominaSettings;
 
 async function IndexRenderer() {
     const app_container = document.getElementById("app-container");
@@ -19,40 +10,7 @@ async function IndexRenderer() {
 
     await loadSettings();
     const app = mount(App, { target: app_container });
-
-    // settings = await window.txnmAPI.settings.Load();
-    // await InitializeTheme();
-    //
-    // await DictionaryUIBuilder.Initialize();
-    //
-    // await LanguageUIBuilder.Initialize();
-    // await GrammaticalClassUIBuilder.Initialize();
-    // await GrammaticalGenreUIBuilder.Initialize();
-    // await EntryUIBuilder.Initialize();
-    //
-    // await SettingUIBuilder.Initialize();
 }
-
-// async function InitializeTheme() {
-//     const settings: TaxonominaSettings = await window.txnmAPI.settings.Expose();
-//     document.body.setAttribute("data-theme", settings.selectedTheme);
-//     document.body.setAttribute("data-variant", settings.themeVariant);
-//     document.body.setAttribute("data-font-size", settings.fontSize);
-//     document.body.classList.toggle("hide-scrollbar", !settings.scrollbarVisibility);
-//     document.querySelector("#help-window-button")!.classList.toggle("inactive", !settings.helpButtonVisibility);
-//     EntryUIBuilder.pageSize = settings.elementsPerPage;
-//     GrammaticalClassUIBuilder.pageSize = settings.elementsPerPage;
-//     GrammaticalGenreUIBuilder.pageSize = settings.elementsPerPage;
-//     LanguageUIBuilder.pageSize = settings.elementsPerPage;
-// }
-
-// export function GetSettings() {
-//     return settings;
-// }
-
-// export function SetSettings(newSettings: TaxonominaSettings) {
-//     settings = newSettings;
-// }
 
 addEventListener("DOMContentLoaded", () => {
     IndexRenderer().catch(console.error);
