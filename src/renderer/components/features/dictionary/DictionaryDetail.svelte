d1bd5b0312b732b44752607d24cef788988d95a3
Noah Boos <nboosgamemaker@gmail.com>
2026-01-31 18:43:38 +0100

    Feature - Added `DictionaryDetail` and `DictionaryList` components.

---
+++ src/renderer/components/features/dictionary/DictionaryDetail.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { I_Dictionary } from "@/shared/interfaces/I_Dictionary";
+    import { currentDictionary } from "@/renderer/stores/currentDictionaryStore";
+    import { Settings2, Trash2 } from "@lucide/svelte";
+
+    interface Props {
+        dictionary: I_Dictionary;
+    }
+
+    let { dictionary }: Props = $props();
+</script>
+
+<style>
+
+</style>
+
+<div class="flex flex-row p-2 justify-between border-2 rounded-lg border-base-50 bg-base-40">
+    <div class="flex flex-col">
+        <p class="text-lg">{ dictionary.name }</p>
+        <p>{ dictionary.description }</p>
+    </div>
+    <div class="flex flex-row gap-2 items-center">
+        {#if dictionary.id !== $currentDictionary?.id}
+            <button onclick={ () => { console.log("Activation du dictionnaire.") } } class="text-base-100 hover:text-base-90 px-4 py-2 border-2 rounded-md border-base-50 hover:border-primary-400 bg-base-35 hover:bg-primary-500 w-fit transition-colors duration-250 ease-out">
+                Activer
+            </button>
+        {/if}
+        <button onclick={ () => { console.log("Modification du dictionnaire.") } } class="text-base-100 hover:text-base-90 p-2 border-2 rounded-md border-base-50 hover:border-primary-400 bg-base-35 hover:bg-primary-500 w-fit transition-colors duration-250 ease-out">
+            <Settings2 />
+        </button>
+        {#if dictionary.id !== $currentDictionary?.id}
+            <button onclick={ () => { console.log("Suppression du dictionnaire.") } } class="text-base-100 hover:text-base-90 p-2 border-2 rounded-md border-base-50 hover:border-primary-400 bg-base-35 hover:bg-primary-500 w-fit transition-colors duration-250 ease-out">
+                <Trash2 />
+            </button>
+        {/if}
+    </div>
+</div>
---
+++ src/renderer/components/features/dictionary/DictionaryList.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import { dictionariesStore, refreshDictionaries } from "@/renderer/stores/dictionariesStore";
+    import { I_Dictionary } from "@/shared/interfaces/I_Dictionary";
+    import Searchbar from "@/renderer/components/ui/forms/Searchbar.svelte";
+    import { Plus } from "@lucide/svelte";
+    import { onMount } from "svelte";
+    import DictionaryDetail from "@/renderer/components/features/dictionary/DictionaryDetail.svelte";
+
+    let query: string = $state('');
+    let filteredDictionaries: I_Dictionary[] = $derived($dictionariesStore.filter(dictionary =>
+        dictionary.name.toLowerCase().includes(query.toLowerCase()) ||
+        dictionary.description?.toLowerCase().includes(query.toLowerCase())
+    ));
+
+    onMount(() => { refreshDictionaries(); });
+
+    $effect(() => { console.log(filteredDictionaries) });
+</script>
+
+<style lang="postcss">
+    @reference '../../../styles/styles.css';
+
+    .add-dictionary-button {
+        @apply flex flex-row gap-2 text-base-100 px-2 py-1 border-2 rounded-md border-primary-300 w-fit transition-colors duration-250 ease-out;
+    }
+
+    .add-dictionary-button:hover {
+        @apply text-base-90 border-primary-400 bg-primary-500;
+    }
+</style>
+
+<div>
+    <h2>
+        Liste des dictionnaires
+    </h2>
+    <div class="space-y-4">
+        <div class="flex flex-row justify-between">
+            <Searchbar bind:query placeholder="Rechercher un dictionnaire..." />
+            <button onclick={ () => { console.log("Ajout d'un dictionnaire."); } } class="add-dictionary-button">
+                <Plus /> Créer un dictionnaire
+            </button>
+        </div>
+        <div class="space-y-2">
+            {#if filteredDictionaries.length === 0}
+                <p>Aucun dictionnaire trouvé{ query ? ' pour la recherche "' + query + '"' : '' }.</p>
+            {:else}
+                {#each filteredDictionaries as dictionary}
+                    <DictionaryDetail dictionary={ dictionary } />
+                {/each}
+            {/if}
+        </div>
+    </div>
+</div>
