4950bb108bb35a6d2ad452e0cb474fa9ae7dbe1c
Noah Boos <nboosgamemaker@gmail.com>
2026-02-01 23:43:05 +0100

    Refactor - Updated the `updateSetting` method for it to save, then to load settings through the API.

--- src/renderer/stores/settingsStore.ts
+++ src/renderer/stores/settingsStore.ts
@@ -9,10 +9,6 @@ export async function loadSettings(): Promise<void> {
 }
 
 export async function updateSetting<K extends keyof I_TaxonominaSettings>(key: K, value: I_TaxonominaSettings[K]): Promise<void> {
-    settings.update((current) => {
-        if (!current) return current;
-        return { ...current, [key]: value };
-    });
-
     await window.txnmAPI.settings.Update(key, value);
+    settings.set(await window.txnmAPI.settings.Load());
 }
