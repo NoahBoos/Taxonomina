59cfba357bdb76c86fcc2fa74a8beebb85378917
Noah Boos <nboosgamemaker@gmail.com>
2026-02-08 18:20:27 +0100

    Feature - Reworked hover state of the content add button, added a selected effect to it.

--- src/renderer/components/browser/AddContentButton.svelte
+++ src/renderer/components/browser/AddContentButton.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
-    import {Plus} from "@lucide/svelte";
+    import { Plus } from "@lucide/svelte";
+    import { currentInspectorStateStore } from "@/renderer/stores/currentInspectorStateStore";
+    import { InspectorAction } from "@/renderer/enums/InspectorAction";
 
     let { onClick }: { onClick: () => void } = $props();
+
+    let is_selected: boolean =  $derived($currentInspectorStateStore.category === 'content' && $currentInspectorStateStore.action === InspectorAction.CREATE);
 </script>
 
 <style lang="postcss">
     @reference '../../styles/styles.css';
 
     .add-content-button {
-        @apply text-base-100 p-1 border-2 rounded-md border-primary-300 w-fit transition-colors duration-250 ease-out;
+        @apply text-base-100 p-1 border-2 rounded-md border-base-40 bg-base-10 w-fit transition-colors duration-250 ease-out;
     }
 
     .add-content-button:hover {
-        @apply text-base-90 border-primary-400 bg-primary-500;
+        @apply bg-accent-400/15 border-accent-500;
+    }
+
+    .selected {
+        @apply border-primary-500 bg-primary-400/15;
     }
 </style>
 
-<button onclick={ onClick } class="add-content-button">
+<button onclick={ onClick } class="add-content-button { is_selected ? 'selected' : '' }">
     <Plus />
 </button>
