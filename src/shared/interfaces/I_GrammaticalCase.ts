8af3b815bcb60194e435ef3269063724b90af1c6
Noah Boos <nboosgamemaker@gmail.com>
2026-02-15 23:39:19 +0100

    Refactor - Deleted unused interfaces to avoid code flooding.

--- src/shared/interfaces/I_GrammaticalCase.ts
+++
@@ -1,4 +0,0 @@
-export interface I_GrammaticalCase {
-    id: number;
-    name: string;
-}
--- src/shared/interfaces/I_Inflection.ts
+++
@@ -1,5 +0,0 @@
-export interface I_Inflection {
-    id: number;
-    entry_id: number;
-    inflection: string;
-}
