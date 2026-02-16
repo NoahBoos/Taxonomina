9200b3e6b26cad2eb1be8adf4082bce38dfaadd1
Noah Boos <nboosgamemaker@gmail.com>
2026-02-16 21:11:01 +0100

    Feature - Added a domain to the `errors/RegistryManager.create` method and removed the `DOMAIN_MAP`.

--- src/shared/errors/RegistryManager.ts
+++ src/shared/errors/RegistryManager.ts
@@ -1,24 +1,12 @@
 import { ErrorDomain, ErrorSeverity, TaxonominaError } from "./types";
 
 export class RegistryManager {
-    public static DOMAIN_MAP: Record<string, ErrorDomain> = {
-        E00: 'common',
-        E01: 'dictionary',
-        E02: 'language',
-        E03: 'entry',
-        E04: 'definition',
-        E05: 'grammatical_class',
-        E06: 'grammatical_genre',
-    }
-
-    public static create<T extends Record<string, { severity: ErrorSeverity, message: string }>>(definitions: T) {
+    public static create<D extends ErrorDomain, T extends Record<string, { severity: ErrorSeverity, message: string }>>(domain: D, definitions: T) {
         const result = {} as {
             [K in keyof T]: TaxonominaError<ErrorDomain>
         };
 
         for (const code in definitions) {
-            const domain: ErrorDomain = this.DOMAIN_MAP[code.slice(0, 3)];
-
             result[code] = {
                 code: code as any,
                 domain: domain,
