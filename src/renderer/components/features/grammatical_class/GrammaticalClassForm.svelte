37eb55694eeecc262c50a2bd6b2d140fce516d33
Noah Boos <nboosgamemaker@gmail.com>
2026-02-19 23:43:05 +0100

    Feature - Added Front-End error management to the grammatical class form.

--- src/renderer/components/features/grammatical_class/GrammaticalClassForm.svelte
+++ src/renderer/components/features/grammatical_class/GrammaticalClassForm.svelte
@@ -7,6 +7,11 @@
     import TextInput from "@/renderer/components/ui/forms/TextInput.svelte";
     import SubmitButton from "@/renderer/components/ui/forms/SubmitButton.svelte";
     import { refreshGrammaticalClasses } from "@/renderer/stores/grammaticalClassesStore";
+    import {
+        grammaticalClassFormErrorsStore, resetGrammaticalClassFormErrors,
+        setGrammaticalClassFormErrors
+    } from "@/renderer/stores/grammaticalClassFormErrorsStore";
+    import { ErrorDomain, TaxonominaError } from "@/shared/errors/types";
 
     const dictionary_id: number = $settings!.currentDictionary;
 
@@ -14,6 +19,8 @@
 
     let is_submitting: boolean = $state(false);
     let submit_button_label: string = $derived(grammatical_class.id === 0 ? "Créer" : "Modifier");
+
+    let name_errors = $derived($grammaticalClassFormErrorsStore.filter((e) => e.target.type === 'form_field' && e.target.field_name === 'name'));
 
     async function loadGrammaticalClass() {
         let inspectorState = $currentInspectorStateStore;
@@ -33,20 +40,26 @@
 
         try {
             const grammaticalClassToSave = $state.snapshot(grammatical_class);
-            const [success, savedGrammaticalGenre] = await GrammaticalClassService.save(grammaticalClassToSave);
-            if (!success || !savedGrammaticalGenre) throw new Error("Failed to save the grammatical class.");
+            const [success, savedGrammaticalGenre, errors] = await GrammaticalClassService.save(grammaticalClassToSave);
+            if (!success || !savedGrammaticalGenre) throw new Error("Failed to save the grammatical class.", { cause: errors });
 
             await refreshGrammaticalClasses();
 
             setCurrentInspectorState(INSPECTOR_STATE_PRESETS.IDLE);
         } catch (error) {
-            console.error("An error occurred while saving the grammatical class : ", error);
+            if (error instanceof Error) {
+                let errors = error.cause as TaxonominaError<ErrorDomain>[];
+                setGrammaticalClassFormErrors(errors);
+            }
         } finally {
             is_submitting = false;
         }
     }
 
-    $effect(() => { loadGrammaticalClass(); });
+    $effect(() => {
+        loadGrammaticalClass();
+        resetGrammaticalClassFormErrors();
+    });
 </script>
 
 <style>
@@ -61,7 +74,7 @@
             <h2>Modifier une classe grammaticale : { grammatical_class.name }</h2>
         {/if}
         <form onsubmit={ onSubmit } class="flex flex-col gap-4">
-            <TextInput name="name" label="Nom" placeholder="Adjectif, mot, verbe... ?" bind:value={ grammatical_class.name } />
+            <TextInput name="name" label="Nom" placeholder="Adjectif, mot, verbe... ?" bind:value={ grammatical_class.name } errors={ name_errors } />
             <SubmitButton label={ submit_button_label } />
         </form>
     </div>
--- src/renderer/services/GrammaticalClassService.ts
+++ src/renderer/services/GrammaticalClassService.ts
@@ -1,4 +1,5 @@
 import {I_GrammaticalClass} from "@/shared/interfaces/I_GrammaticalClass";
+import { ErrorDomain, TaxonominaError } from "@/shared/errors/types";
 
 export class GrammaticalClassService {
     public static async readAll(dictionary_id: number): Promise<I_GrammaticalClass[]> {
@@ -13,11 +14,11 @@ export class GrammaticalClassService {
         return await window.txnmAPI.repositories.grammaticalClass.readOne(grammatical_class_id);
     }
 
-    public static async save(grammatical_class: I_GrammaticalClass): Promise<[boolean, I_GrammaticalClass | undefined]> {
-        let [success, saved_grammatical_class] = grammatical_class.id == 0
+    public static async save(grammatical_class: I_GrammaticalClass): Promise<[boolean, I_GrammaticalClass | undefined, TaxonominaError<ErrorDomain>[]]> {
+        let [success, saved_grammatical_class, errors] = grammatical_class.id == 0
             ? await window.txnmAPI.repositories.grammaticalClass.create(grammatical_class)
             : await window.txnmAPI.repositories.grammaticalClass.update(grammatical_class);
-        return [success, saved_grammatical_class];
+        return [success, saved_grammatical_class, errors];
     }
 
     public static async delete(grammatical_class_id: number): Promise<boolean> {
