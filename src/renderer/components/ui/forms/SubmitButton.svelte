836ee07702ce2ff9f876096590fbc7e750a692df
Noah Boos <nboosgamemaker@gmail.com>
2026-02-25 18:41:54 +0100

    Refactor - Added an `onClick` and a `variant` props to the `SubmitButton` component.

--- src/renderer/components/ui/forms/SubmitButton.svelte
+++ src/renderer/components/ui/forms/SubmitButton.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-    let { label }: { label: string } = $props();
+    interface Props {
+        label: string;
+        onClick?: () => void;
+        variant?: 'uncentered' | 'centered';
+    }
+
+    let { label, onClick, variant = 'centered' }: Props = $props();
 </script>
 
 <style lang="postcss">
     @reference '../../../styles/styles.css';
 
     button {
-        @apply block font-semibold px-4 py-2 mx-auto border-2 rounded-md border-primary-500 bg-transparent w-fit transition-colors duration-250 ease-out;
+        @apply block font-semibold px-4 py-2 border-2 rounded-md border-primary-500 bg-transparent w-fit transition-colors duration-250 ease-out;
     }
 
     button:hover {
@@ -14,6 +20,6 @@
     }
 </style>
 
-<button type="submit">
+<button type="submit" onclick={ onClick } class="{ variant === 'centered' ? 'mx-auto' : '' }">
     { label }
 </button>
