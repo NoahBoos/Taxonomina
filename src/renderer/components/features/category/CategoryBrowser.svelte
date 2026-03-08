630245abe3e6add745624361116f65c9a8dbddb7
Noah Boos <nboosgamemaker@gmail.com>
2026-03-08 14:48:19 +0100

    Patch - Replaced category browser's previous title by "Catégories".

--- src/renderer/components/features/category/CategoryBrowser.svelte
+++ src/renderer/components/features/category/CategoryBrowser.svelte
@@ -34,7 +34,7 @@
 
 <div class="flex flex-col h-full gap-4">
     <div class="space-y-2">
-        <h2>Classes grammaticales</h2>
+        <h2>Catégories</h2>
         <div class="flex flex-row items-center gap-2">
             <ContentSearchBar bind:query={ query } bind:currentPage={ currentPage } />
             <AddContentButton onClick={ openCreateForm } contentType={ ContentType.Category } />
