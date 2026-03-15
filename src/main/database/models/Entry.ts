774871fffbebb6060bc68f33b4b309a4abe94df6
Noah Boos <nboosgamemaker@gmail.com>
2026-03-15 21:44:57 +0100

    Feature - Added relations to the entry model.

--- src/main/database/models/Entry.ts
+++ src/main/database/models/Entry.ts
@@ -1,13 +1,22 @@
 import {I_Entry} from "../../../shared/interfaces/I_Entry";
 import { ErrorDomain, TaxonominaError } from "../../../shared/errors/types";
 import { ENTRY_ERROR_REGISTRY } from "../../../shared/errors/registries/entryErrorRegistry";
+import { Language } from "./Language";
+import { Definition } from "./Definition";
+import { GrammaticalClass } from "./GrammaticalClass";
+import { GrammaticalGenre } from "./GrammaticalGenre";
 
 export class Entry {
     constructor(
         public readonly id: number,
         public readonly dictionary_id: number,
         public language_id: number,
-        private _lemma: string
+        private _lemma: string,
+        public language: Language | undefined = undefined,
+        public grammatical_classes: GrammaticalClass[] | undefined = undefined,
+        public grammatical_genres: GrammaticalGenre [] | undefined = undefined,
+        public translations: Entry[] | undefined = undefined,
+        public definitions: Definition[] | undefined = undefined,
     ) {
         this.lemma = _lemma;
     }
@@ -25,16 +34,26 @@ export class Entry {
             id: this.id,
             dictionary_id: this.dictionary_id,
             language_id: this.language_id,
-            lemma: this.lemma
+            lemma: this.lemma,
+            language: this.language?.toJSON(),
+            grammatical_classes: this.grammatical_classes?.map(gc => gc.toJSON()),
+            grammatical_genres: this.grammatical_genres?.map(gg => gg.toJSON()),
+            translations: this.translations?.map(t => t.toJSON()),
+            definitions: this.definitions?.map(d => d.toJSON()),
         }
     }
 
-    public static hydrate(raw: I_Entry) {
+    public static hydrate(raw: I_Entry): Entry {
         return new Entry(
             raw.id,
             raw.dictionary_id,
             raw.language_id,
             raw.lemma,
+            raw.language ? Language.hydrate(raw.language) : undefined,
+            raw.grammatical_classes ? raw.grammatical_classes.map(gc => GrammaticalClass.hydrate(gc)) : undefined,
+            raw.grammatical_genres ? raw.grammatical_genres.map(gg => GrammaticalGenre.hydrate(gg)) : undefined,
+            raw.translations ? raw.translations.map(t => Entry.hydrate(t)) : undefined,
+            raw.definitions ? raw.definitions.map(d => Definition.hydrate(d)) : undefined
         );
     }
 
