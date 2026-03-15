d2fe46c7a5786a49b89361e940ce75842c4c38f2
Noah Boos <nboosgamemaker@gmail.com>
2026-03-15 19:41:00 +0100

    Feature - Added relations to the entry interface.

--- src/shared/interfaces/I_Entry.ts
+++ src/shared/interfaces/I_Entry.ts
@@ -1,6 +1,16 @@
+import { I_Language } from "./I_Language";
+import { I_Definition } from "./I_Definition";
+import { I_GrammaticalClass } from "./I_GrammaticalClass";
+import { I_GrammaticalGenre } from "./I_GrammaticalGenre";
+
 export interface I_Entry {
     id: number;
     dictionary_id: number;
     language_id: number;
     lemma: string;
+    language: I_Language | undefined;
+    grammatical_classes: I_GrammaticalClass[] | undefined;
+    grammatical_genres: I_GrammaticalGenre[] | undefined;
+    translations: I_Entry[] | undefined;
+    definitions: I_Definition[] | undefined;
 }
