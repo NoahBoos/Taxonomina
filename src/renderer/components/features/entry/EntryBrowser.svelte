82753e61317c5f23ca55150e322d1330dd422c46
Noah Boos <nboosgamemaker@gmail.com>
2026-03-25 22:45:56 +0100

    Feature - Added language filter to the entry browser.

--- src/renderer/components/features/entry/EntryBrowser.svelte
+++ src/renderer/components/features/entry/EntryBrowser.svelte
@@ -11,12 +11,20 @@
     import AddContentButton from "@/renderer/components/browser/AddContentButton.svelte";
     import { entriesStore, refreshEntries } from "@/renderer/stores/entriesStore";
     import { ContentType } from "@/renderer/enums/ContentType";
+    import { SelectOptions } from "@/renderer/types/SelectOptions";
+    import { LANGUAGES } from "@/renderer/utils/selectOptionPresets";
 
     const elementsPerPage: number = $derived($settings?.elementsPerPage ?? 25);
     let currentPage: number = $state(1);
 
     let query: string = $state('');
-    let filteredEntries: I_Entry[] = $derived($entriesStore.filter(entry => entry.lemma.toLowerCase().includes(query.toLowerCase())));
+    let language_id: number = $state(0);
+    let filteredEntries: I_Entry[] = $derived($entriesStore.filter(entry =>
+        entry.lemma.toLowerCase().includes(query.toLowerCase())
+        && (language_id === 0 || entry.language_id === language_id)
+    ));
+
+    let languageOptions = $state<SelectOptions>({});
 
     let totalPages: number = $derived(Math.ceil(filteredEntries.length / elementsPerPage));
     let paginatedEntries: I_Entry[] = $derived(filteredEntries.slice((currentPage - 1) * elementsPerPage, currentPage * elementsPerPage));
@@ -25,11 +33,18 @@
         await refreshEntries();
     }
 
+    async function resetPagination() {
+        currentPage = 1;
+    }
+
     function openCreateForm() {
         setCurrentInspectorState(INSPECTOR_STATE_PRESETS.CONTENT.ENTRY.CREATE);
     }
 
-    $effect(() => { refresh(); });
+    $effect(() => {
+        refresh();
+        LANGUAGES($settings!.currentDictionary).then(options => languageOptions = options);
+    });
 </script>
 
 <style>
@@ -43,6 +58,11 @@
             <ContentSearchBar bind:query={ query } bind:currentPage={ currentPage } />
             <AddContentButton onClick={ openCreateForm } contentType={ ContentType.Entry } />
         </div>
+        <select id="language_id" bind:value={ language_id } onchange={ resetPagination } class="w-full px-2 py-1 border-2 rounded-lg border-base-40 hover:border-accent-500 bg-base-10 hover:bg-base-20 transition-colors duration-250 ease-out">
+            {#each Object.entries(languageOptions) as [option_value, option_label]}
+                <option value={ Number(option_value) }>{ option_label }</option>
+            {/each}
+        </select>
     </div>
     <ContentList items={ paginatedEntries } />
     <div class="flex flex-row justify-between items-center">
